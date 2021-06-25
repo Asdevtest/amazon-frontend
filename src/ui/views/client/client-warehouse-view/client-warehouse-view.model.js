c2112d51037ca1be4d530e45418e27cd1d6eb71a
Aleksey Logvenkin <alogvenkin@yandex.ru>
2021-06-25 18:37:31 +0300

    Connect api buyer warehouse tasks 81

--- src/ui/views/client/client-warehouse-view/client-warehouse-view.model.js
+++ src/ui/views/client/client-warehouse-view/client-warehouse-view.model.js
@@ -2,22 +2,24 @@ import {makeAutoObservable, runInAction} from 'mobx'
 
 import {loadingStatuses} from '@constants/loading-statuses'
 
-import {ClientModel} from '@models/client-model'
+import {BoxesModel} from '@models/boxes-model'
 
 export class ClientWarehouseViewModel {
   history = undefined
   requestStatus = undefined
   error = undefined
 
+  boxesMy = []
+
   drawerOpen = false
   curPage = 1
   rowsPerPage = 5
-  boxes = []
-  selectedBoxes = ['2096c_box']
+  selectedBoxes = []
+
   showSendOwnProductModal = false
   showEditBoxModal = false
   showRedistributeBoxModal = false
-  modalRedistributeBoxAddNewBox = undefined
+  showRedistributeBoxAddNewBoxModal = false
   showRedistributeBoxSuccessModal = false
 
   constructor({history}) {
@@ -25,74 +27,191 @@ export class ClientWarehouseViewModel {
     makeAutoObservable(this, undefined, {autoBind: true})
   }
 
-  loadData() {
-    try {
-      this.setRequestStatus(loadingStatuses.isLoading)
-      this.getBoxes()
-      this.setRequestStatus(loadingStatuses.success)
-    } catch (error) {
-      this.setRequestStatus(loadingStatuses.failed)
-      console.log(error)
-      if (error.body && error.body.message) {
-        this.error = error.body.message
-      }
-    }
-  }
-
-  getBoxes() {
-    try {
-      const result = ClientModel.getBoxes()
-      runInAction(() => {
-        this.boxes = result
-      })
-    } catch (error) {
-      console.log(error)
-      if (error.body && error.body.message) {
-        this.error = error.body.message
-      }
-    }
+  onModalRedistributeBoxAddNewBox(value) {
+    this.modalRedistributeBoxAddNewBox = value
   }
 
   onTriggerDrawer() {
     this.drawerOpen = !this.drawerOpen
   }
 
-  onChangeCurPage(e, value) {
+  onChangeCurPage = (e, value) => {
     this.curPage = value
   }
 
-  onChangeRowsPerPage(e) {
+  onChangeRowsPerPage = e => {
     this.rowsPerPage = Number(e.target.value)
-    this.curPage = 1
+    this.paginationPage = 1
   }
 
-  onTriggerCheckbox(boxId) {
-    const {selectedBoxes} = this.state
-
-    const updatedselectedBoxes = selectedBoxes.includes(boxId)
-      ? selectedBoxes.filter(id => id !== boxId)
-      : selectedBoxes.concat(boxId)
+  onTriggerCheckbox = boxId => {
+    const updatedselectedBoxes = this.selectedBoxes.includes(boxId)
+      ? this.selectedBoxes.filter(_id => _id !== boxId)
+      : this.selectedBoxes.concat(boxId)
     this.selectedBoxes = updatedselectedBoxes
   }
 
-  onRedistribute(updatedBoxes) {
-    this.boxes = updatedBoxes
+  onRedistribute(id, updatedBoxes) {
+    const boxes = updatedBoxes.map(el => el.items)
+    this.splitBoxes(id, boxes)
     this.selectedBoxes = []
   }
 
+  onEditBoxSubmit(id, data) {
+    this.updateBox(id, data)
+  }
+
   onClickMerge() {
-    alert('Box merging')
+    this.mergeBoxes(this.selectedBoxes)
   }
 
-  onTriggerModal(modalKey) {
-    this[modalKey] = !this.modalKey
+  onTriggerOpenModal(modalState) {
+    this[modalState] = !this[modalState]
   }
 
-  onRedistributeBoxAddNewBox(value) {
-    this.modalRedistributeBoxAddNewBox = value
+  async loadData() {
+    try {
+      this.setRequestStatus(loadingStatuses.isLoading)
+      await this.getBoxesMy()
+      this.setRequestStatus(loadingStatuses.success)
+    } catch (error) {
+      console.log(error)
+      this.setRequestStatus(loadingStatuses.failed)
+    }
+  }
+
+  async mergeBoxes(boxes) {
+    try {
+      const result = await BoxesModel.mergeBoxes(boxes)
+
+      console.log(result, 'RESULT')
+
+      await this.getBoxesMy()
+
+      this.selectedBoxes = []
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async splitBoxes(id, data) {
+    try {
+      await BoxesModel.splitBoxes(id, data)
+
+      await this.getBoxesMy()
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async updateBox(id, data) {
+    try {
+      await BoxesModel.updateBox(id, data)
+
+      await this.getBoxesMy()
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async getBoxesMy() {
+    try {
+      const result = await BoxesModel.getBoxes()
+
+      console.log(result, 'BOoooooooooooooooooooooXES DATA')
+
+      runInAction(() => {
+        this.boxesMy = result
+      })
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async approveBoxesOperation(id) {
+    try {
+      await BoxesModel.approveBoxesOperation(id)
+
+      await this.getBoxesMy()
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async cancelMergeBoxes(id) {
+    try {
+      await BoxesModel.cancelMergeBoxes(id)
+
+      await this.getBoxesMy()
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  async cancelSplitBoxes(id) {
+    try {
+      await BoxesModel.cancelSplitBoxes(id)
+
+      await this.getBoxesMy()
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
   }
 
   setRequestStatus(requestStatus) {
     this.requestStatus = requestStatus
+  }
+
+  //  для тестов
+  async removeBox(id) {
+    try {
+      await BoxesModel.removeBox(id)
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
+  }
+
+  //  для тестов
+  async createBox() {
+    try {
+      await BoxesModel.createBox({
+        lengthCm: 10,
+        widthCm: 10,
+        heightCm: 10,
+        weighGrossKg: 15.5,
+        volumeWeightKg: 25.5,
+        weightFinalAccountingKg: 25.5,
+        lengthCmSupplier: 25,
+        widthCmSupplier: 35,
+        heightCmSupplier: 45,
+        weighGrossKgSupplier: 45,
+        volumeWeightKgSupplier: 15,
+        weightFinalAccountingKgSupplier: 25,
+        warehouse: 25,
+        deliveryMethod: 25,
+        scheduledDispatchDate: '2021-06-24',
+        factDispatchDate: '2021-06-24',
+        isDraft: false,
+        items: [
+          {
+            product: '607dceac3551e3fa7e7fbb69',
+            amount: '39',
+            order: '607dceac3551e3fa7e7fbb69',
+          },
+        ],
+        clientId: '607dceac3551e3fa7e7fbb69',
+      })
+    } catch (error) {
+      console.log(error)
+      this.error = error
+    }
   }
 }
