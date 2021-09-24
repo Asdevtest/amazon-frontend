e64c168b0d13b8ad23c881acb9320fe5fd5a9379
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2021-09-24 12:56:35 +0300

    hotfix: admin-warehouse-boxes / warehouse-dashboard / warehouse-vacant-tasks

--- src/ui/components/table-columns/admin/boxes-columns/boxes-columns.jsx
+++ src/ui/components/table-columns/admin/boxes-columns/boxes-columns.jsx
@@ -7,7 +7,7 @@ import {
   FinalWeightCell,
   IdCell,
   NoActiveBarcodeCell,
-  NormDateWithParseISOCell,
+  NormDateCell,
   OrderCell,
   PriceCell,
   renderFieldValueCell,
@@ -23,7 +23,7 @@ export const adminBoxesViewColumns = () => [
   {
     field: 'createdAt',
     headerName: textConsts.createdAtField,
-    renderCell: params => <NormDateWithParseISOCell params={params} />,
+    renderCell: params => <NormDateCell params={params} />,
     width: 150,
     type: 'date',
   },
