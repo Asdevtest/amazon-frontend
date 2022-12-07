9ece63fc8162f18eef633db4c10fd1114fc3b4a6
Vlad <vladis67948@gmail.com>
2022-12-07 09:04:43 +0300

    admin

--- src/ui/views/admin/admin-warehouse-views/admin-warehouse-view/admin-warehouse-view.model.js
+++ src/ui/views/admin/admin-warehouse-views/admin-warehouse-view/admin-warehouse-view.model.js
@@ -1,4 +1,4 @@
-import {makeAutoObservable} from 'mobx'
+import {makeAutoObservable, runInAction} from 'mobx'
 
 export class AdminWarehouseViewModel {
   history = undefined
@@ -8,12 +8,16 @@ export class AdminWarehouseViewModel {
   drawerOpen = false
 
   constructor({history}) {
-    this.history = history
+    runInAction(() => {
+      this.history = history
+    })
     makeAutoObservable(this, undefined, {autoBind: true})
   }
 
   onChangeDrawerOpen() {
-    this.drawerOpen = !this.drawerOpen
+    runInAction(() => {
+      this.drawerOpen = !this.drawerOpen
+    })
   }
 
   onClickTasks() {
