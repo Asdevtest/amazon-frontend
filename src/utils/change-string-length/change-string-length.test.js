a0f25e521849bb801aaf3c274669c70fd0865039
Aleksey Logvenkin <alogvenkin@yandex.ru>
2023-01-17 15:14:18 +0600

    test commit

--- src/utils/change-string-length/change-string-length.test.js
+++ src/utils/change-string-length/change-string-length.test.js
@@ -1,9 +1,9 @@
-// describe('Test suite one', () => {
-//   test('Test 1', () => {
-//     expect(Math.max(1, 5, 10)).toBe(10)
-//   })
+describe('Test suite one', () => {
+  test('Test 1', () => {
+    expect(Math.max(1, 5, 10)).toBe(10)
+  })
 
-//   test('Test 2', () => {
-//     expect(Math.max(1, 5, 10)).not.toBe(5)
-//   })
-// })
+  test('Test 2', () => {
+    expect(Math.max(1, 5, 10)).not.toBe(5)
+  })
+})
