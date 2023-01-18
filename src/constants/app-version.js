bce748af53f7d1876be6c35f6749bd7c5cfb9d01
Aleksey Logvenkin <alogvenkin@yandex.ru>
2023-01-18 15:50:59 +0600

    tests

--- src/constants/app-version.js
+++ src/constants/app-version.js
@@ -1,1 +1,1 @@
-export const appVersion = 'v2.12.389' // КАЖДЫЙ РАЗ ОБНОВЛЯЙ И В manifest.json
+export const appVersion = 'v2.12.390' // КАЖДЫЙ РАЗ ОБНОВЛЯЙ И В manifest.json
--- src/utils/change-string-length/change-string-length.test.js
+++ src/utils/change-string-length/change-string-length.test.js
@@ -1,15 +1,22 @@
-// import {getShortenStringIfLongerThanCount} from './change-string-length'
+import {getShortenStringIfLongerThanCount} from './change-string-length'
 
-// const {getShortenStringIfLongerThanCount} = require('./change-string-length')
+describe('Test getShortenStringIfLongerThanCount(str, count)', () => {
+  test('normal props', () => {
+    const res = getShortenStringIfLongerThanCount('qwerty', 2)
 
-describe('Test getShortenStringIfLongerThanCount()', () => {
-  // test('Test 1', () => {
-  //   const res = getShortenStringIfLongerThanCount('qwerty', 2)
-
-  //   expect(res).toBe('qw...')
-  // })
-
-  test('Test 2', () => {
-    expect(Math.max(1, 5, 10)).not.toBe(5)
+    expect(res).toBe('qw...')
+    expect(typeof res).toBe('string')
   })
+
+  test('str short than count', () => {
+    const res = getShortenStringIfLongerThanCount('qwerty', 2)
+
+    expect(res).toBe('qw...')
+    expect(typeof res).toBe('string')
+  })
+
+  // первый пример
+  // test('Test 2', () => {
+  //   expect(Math.max(1, 5, 10)).not.toBe(5)
+  // })
 })
