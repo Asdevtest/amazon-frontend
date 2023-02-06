7c4747c0ce285fac8a79512c266a7ce452407463
Vlad <vladis67948@gmail.com>
2023-02-06 15:16:41 +0300

    get-amazon-code-from-link.test.js

--- src/utils/get-amazon-code-from-link.js
+++
@@ -1,6 +0,0 @@
-export const getAmazonCodeFromLink = link => {
-  const pattern = /\/(product|dp)\/\w+/
-  const out = link.match(pattern) ? link.match(pattern) : []
-  const code = out[0] ? out[0].split('/')[2] : ''
-  return code
-}
---
+++ src/utils/get-amazon-code-from-link/index.js
@@ -0,0 +1,1 @@
+export * from './get-amazon-code-from-link.js'
