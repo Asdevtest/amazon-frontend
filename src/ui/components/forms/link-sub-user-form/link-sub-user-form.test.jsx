f6b6696080895bbf211ef1177028c80d41ad5b9e
Vlad <vladis67948@gmail.com>
2023-02-08 14:29:50 +0300

    tests

---
+++ src/ui/components/forms/link-sub-user-form/link-sub-user-form.test.jsx
@@ -0,0 +1,14 @@
+// import {render, screen} from '@testing-library/react'
+
+// import {LinkSubUserForm} from './link-sub-user-form'
+
+// const closeModal = jest.fn()
+// const onSubmit = jest.fn()
+
+// describe('LinkSubUserForm({closeModal, onSubmit})', () => {
+//   test('Renders LinkSubUserForm', () => {
+//     render(<LinkSubUserForm closeModal={closeModal} onSubmit={onSubmit} />)
+
+//     expect(screen.getAllByText(/Add a sub-user/i)).toBeInTheDocument()
+//   })
+// })
