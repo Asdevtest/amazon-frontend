e0c253e5e07134926f365ca9224d715c1066c76a
Vlad <vladis67948@gmail.com>
2023-02-08 08:38:13 +0300

    test

---
+++ src/utils/get-amazon-image-url/get-amazon-image-url.test.js
@@ -0,0 +1,27 @@
+// import { getAmazonImageUrl } from "./get-amazon-image-url"
+
+// describe('Test getAmazonImageUrl(str, big)', () => {
+//   const validTestValue = [
+//     {str: 10.6,  expect: 10.5},
+//     {enter: 10.8, expect: 11},
+//   ]
+
+// const unvalidTestValue = [
+//   {enter: '10.6', expect: 10.5},
+//   {enter: '10.8', expect: 11},
+// ]
+
+// validTestValue.forEach(value => {
+//   test('Valid props', () => {
+//     expect(getAmazonImageUrl(value.enter)).toBe(value.expect)
+//     expect(typeof res).toBe('number')
+//   })
+// })
+
+// unvalidTestValue.forEach(value => {
+//   test('Unvalid props', () => {
+//     expect(getAmazonImageUrl(value.enter)).toBe(value.expect)
+//     expect(typeof res).toBe('number')
+//   })
+// })
+// })
