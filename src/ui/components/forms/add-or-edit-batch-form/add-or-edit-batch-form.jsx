e3eb59e91d0010bf211699bd9f564f8255c656ba
Vlad <vladis67948@gmail.com>
2023-02-23 17:18:05 +0300

    5324: line text changed

--- src/ui/components/forms/add-or-edit-batch-form/add-or-edit-batch-form.jsx
+++ src/ui/components/forms/add-or-edit-batch-form/add-or-edit-batch-form.jsx
@@ -541,7 +541,7 @@ export const AddOrEditBatchForm = observer(
                 Footer: () => (
                   <div className={classNames.boxCounterWrapper}>
                     <Typography className={classNames.boxCounterText}>
-                      {t(TranslationKey['Quantity of boxes in batch']) + ':'}
+                      {t(TranslationKey['Quantity of boxes']) + ':'}
                     </Typography>
                     <Typography className={classNames.boxCounterCount}>
                       {chosenBoxes.reduce((ac, cur) => (ac += cur.originalData.amount), 0)}
