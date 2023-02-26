a4cf5b1064fc40a3c265db7cbfe2bcfbf0e895df
Vlad <vladis67948@gmail.com>
2023-02-26 11:47:20 +0300

    added new column asin for freelance my requests

--- src/services/rest-api-service/codegen/src/model/InlineObject36.js
+++ src/services/rest-api-service/codegen/src/model/InlineObject36.js
@@ -22,11 +22,10 @@ class InlineObject36 {
     /**
      * Constructs a new <code>InlineObject36</code>.
      * @alias module:model/InlineObject36
-     * @param isFormed {Boolean} Сформирована ли коробка
      */
-    constructor(isFormed) { 
+    constructor() { 
         
-        InlineObject36.initialize(this, isFormed);
+        InlineObject36.initialize(this);
     }
 
     /**
@@ -34,8 +33,7 @@ class InlineObject36 {
      * This method is used by the constructors of any subclasses, in order to implement multiple inheritance (mix-ins).
      * Only for internal use.
      */
-    static initialize(obj, isFormed) { 
-        obj['isFormed'] = isFormed;
+    static initialize(obj) { 
     }
 
     /**
