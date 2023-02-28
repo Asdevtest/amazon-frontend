57cae8baa739e33d0d5b4925c810712df77373ef
Vlad <vladis67948@gmail.com>
2023-02-28 12:03:09 +0300

    push

--- src/services/rest-api-service/codegen/src/model/InlineObject18.js
+++ src/services/rest-api-service/codegen/src/model/InlineObject18.js
@@ -38,8 +38,12 @@ class InlineObject18 {
     static initialize(obj, buyerComment) { 
         obj['buyerComment'] = buyerComment;
     }
+
     /**
      * Constructs a <code>InlineObject18</code> from a plain JavaScript object, optionally creating a new instance.
+     * Copies all relevant properties from <code>data</code> to <code>obj</code> if supplied or a new instance if not.
+     * @param {Object} data The plain JavaScript object bearing properties of interest.
+     * @param {module:model/InlineObject18} obj Optional instance to populate.
      * @return {module:model/InlineObject18} The populated <code>InlineObject18</code> instance.
      */
     static constructFromObject(data, obj) {
