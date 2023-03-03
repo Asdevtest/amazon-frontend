8276af11c02b6db808c4190c20e911ffac009ae0
Vlad <vladis67948@gmail.com>
2023-03-03 16:25:21 +0300

    table styling

--- src/ui/views/shared/services-detail-view/services-detail-view.style.js
+++ src/ui/views/shared/services-detail-view/services-detail-view.style.js
@@ -122,4 +122,40 @@ export const styles = theme => ({
   acceptMessage: {
     color: '#00B746',
   },
+
+  dataGridWrapper: {
+    marginTop: 30,
+    height: '55vh',
+    backgroundColor: theme.palette.background.general,
+    // boxShadow: 0px 2px 10px 2px rgba(190, 190, 190, 0.15),
+    borderRadius: 4,
+  },
+
+  row: {
+    cursor: 'url(/assets/icons/Cursor.svg) 4 12, auto',
+    transition: '0.3s ease',
+    '&:hover': {
+      transform: 'scale(1.01)',
+    },
+  },
+  root: {
+    border: '0 !important',
+    boxShadow: '0px 2px 10px 2px rgba(190, 190, 190, 0.15)',
+    // backgroundColor: theme.palette.background.general,
+
+    backgroundColor: theme.palette.background.general,
+  },
+  footerContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    borderTop: 'none !important',
+  },
+  footerCell: {
+    padding: 0,
+    margin: 0,
+  },
+  toolbarContainer: {
+    height: '52px',
+  },
 })
