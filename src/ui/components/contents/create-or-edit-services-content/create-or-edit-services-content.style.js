09204efdbbe34fc9bf89942c22711e5c032db328
Vlad <vladis67948@gmail.com>
2023-04-06 12:01:58 +0300

    styles fixed

--- src/ui/components/contents/create-or-edit-services-content/create-or-edit-services-content.style.js
+++ src/ui/components/contents/create-or-edit-services-content/create-or-edit-services-content.style.js
@@ -13,7 +13,7 @@ export const useClassNames = makeStyles()(theme => ({
 
     background: theme.palette.background.general,
 
-    boxShadow: '0px 2px 10px 2px rgba(190, 190, 190, 0.15)',
+    boxShadow: `0px 2px 8px 2px ${theme.palette.boxShadow.general}`,
     borderRadius: 4,
 
     display: 'flex',
