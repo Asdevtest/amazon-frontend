ec3da2d7011433beee858a21e92f531c5c0919ed
Aleksey Logvenkin <alogvenkin@yandex.ru>
2023-04-11 10:04:46 +0600

    fix(resultModal): fix_view_actions_btns

--- src/ui/components/forms/request-designer-result-client-form/request-designer-result-client-form.jsx
+++ src/ui/components/forms/request-designer-result-client-form/request-designer-result-client-form.jsx
@@ -52,11 +52,14 @@ export const RequestDesignerResultClientForm = ({
 }) => {
   const {classes: classNames} = useClassNames()
 
-  console.log('request', request)
+  // console.log('request', request)
 
-  console.log('proposal', proposal)
+  // console.log('proposal', proposal)
 
-  const isNotClient = userInfo._id !== request.request.createdBy._id
+  console.log('userInfo', userInfo)
+
+  const isNotClient =
+    userInfo._id !== request.request.createdBy._id && userInfo.masterUser?._id !== request.request.createdBy._id
 
   const proposalIsAccepted = [
     RequestProposalStatus.ACCEPTED_BY_CLIENT,
