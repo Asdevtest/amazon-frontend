d7e2b4e9445c8e504494baa79b9b11cd48212ecf
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-04-20 21:02:22 +0300

    fix(warehouse): Change button color text in modal

--- src/ui/components/forms/edit-box-storekeeper-form/edit-box-storekeeper-form.style.js
+++ src/ui/components/forms/edit-box-storekeeper-form/edit-box-storekeeper-form.style.js
@@ -230,7 +230,7 @@ export const useClassNames = makeStyles()(theme => ({
   storekeeperBtn: {
     height: '32px',
     width: '230px',
-    color: theme.palette.text.negativeMain,
+    color: theme.palette.type === 'light' && theme.palette.text.negativeMain,
   },
 
   sizesTitleWrapper: {
