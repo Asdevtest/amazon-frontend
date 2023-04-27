c1c0006d83bee82fc655d4f9610b861ca2a8ec10
Aleksey Logvenkin <alogvenkin@yandex.ru>
2023-04-27 16:11:57 +0600

    fix(myProposals): fix_bsd

--- src/ui/components/forms/request-designer-result-client-form/request-designer-result-client-form.jsx
+++ src/ui/components/forms/request-designer-result-client-form/request-designer-result-client-form.jsx
@@ -188,6 +188,9 @@ export const RequestDesignerResultClientForm = ({
 }) => {
   const {classes: classNames} = useClassNames()
 
+  console.log('request', request)
+  console.log('proposal', proposal)
+
   const isNotClient =
     userInfo._id !== request.request.createdBy._id && userInfo.masterUser?._id !== request.request.createdBy._id
 
@@ -343,8 +346,12 @@ export const RequestDesignerResultClientForm = ({
             containerClasses={classNames.containerField}
             inputComponent={
               <Typography className={cx(classNames.simpleSpan /* , classNames.textMargin */)}>
-                <a target="_blank" rel="noreferrer" href={`https://www.amazon.com/dp/${proposal.request.asin}`}>
-                  <span className={classNames.linkSpan}>{proposal.request.asin}</span>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={`https://www.amazon.com/dp/${proposal?.request?.asin || request.request.asin}`}
+                >
+                  <span className={classNames.linkSpan}>{proposal?.request?.asin || request.request.asin}</span>
                 </a>
               </Typography>
             }
