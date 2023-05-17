2e28a143ece3558d68d442168d3cf35cf9b4b61f
Vlad <vladis67948@gmail.com>
2023-05-17 15:36:26 +0300

    fix

--- src/ui/components/chat/chat/chat-messages-list/chat-messages/chat-message-files/chat-message-files.styles.ts
+++ src/ui/components/chat/chat/chat-messages-list/chat-messages/chat-message-files/chat-message-files.styles.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {makeStyles} from 'tss-react/mui'
 
 export const useChatMessageFileStyles = makeStyles()(theme => ({
@@ -8,4 +7,44 @@ export const useChatMessageFileStyles = makeStyles()(theme => ({
     gap: '10px',
     flexDirection: 'column',
   },
+
+  fileWrapper: {
+    display: 'flex',
+    gap: '10px',
+    cursor: 'pointer',
+  },
+
+  logo: {
+    width: '30px',
+    height: '40px',
+
+    img: {
+      width: '100%',
+      height: '100%',
+      objectFit: 'cover',
+    },
+  },
+
+  info: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '3px',
+
+    p: {
+      fontSize: 14,
+      lineHeight: 1,
+      color: theme.palette.text.general,
+    },
+  },
+  fileName: {
+    maxWidth: 160,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  },
+  fileSize: {
+    color: theme.palette.text.second,
+    fontSize: 12,
+  },
+  fileType: {},
 }))
