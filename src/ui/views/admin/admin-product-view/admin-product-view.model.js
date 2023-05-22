30b23f1edaba80d875e251c94624316e00dd0057
Vlad <vladis67948@gmail.com>
2023-05-22 14:01:40 +0300

    fix(model): new logic added

--- src/ui/views/admin/admin-product-view/admin-product-view.model.js
+++ src/ui/views/admin/admin-product-view/admin-product-view.model.js
@@ -37,7 +37,7 @@ export class AdminProductViewModel {
   requestStatus = undefined
   actionStatus = undefined
 
-  inInventory = undefined
+  // inInventory = undefined
 
   productId = undefined
   product = undefined
@@ -71,17 +71,19 @@ export class AdminProductViewModel {
     return SettingsModel.languageTag
   }
 
-  constructor({ history, location }) {
+  constructor({ history /* , location */ }) {
+    const url = new URL(window.location.href)
+
     runInAction(() => {
       this.history = history
-      this.productId = history.location.search.slice(1)
+      this.productId = url.searchParams.get('product-id')
     })
 
-    if (location.state) {
-      runInAction(() => {
-        this.inInventory = location.state.inInventory
-      })
-    }
+    // if (location.state) {
+    //   runInAction(() => {
+    //     this.inInventory = location.state.inInventory
+    //   })
+    // }
     makeAutoObservable(this, undefined, { autoBind: true })
 
     reaction(
