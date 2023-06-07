c0b63dcba9d4802d7dc766ab44cc18e88c8d476e
oltree <o.melex@bk.ru>
2023-06-07 09:01:53 +0300

    6240-fix(torekeeperTasks):fixed svg icons and added styles

--- src/ui/views/warehouse/warehouse-tasks-views/warehouse-vacant-tasks-view/warehouse-vacant-tasks-view.jsx
+++ src/ui/views/warehouse/warehouse-tasks-views/warehouse-vacant-tasks-view/warehouse-vacant-tasks-view.jsx
@@ -31,6 +31,7 @@ import { t } from '@utils/translations'
 import { WarehouseVacantViewModel } from './warehouse-vacant-tasks-view.model'
 import { styles } from './warehouse-vacant-tasks-view.style'
 import { Button } from '@components/shared/buttons/button'
+import { DownloadIcon } from '@components/shared/svg-icons'
 
 export const WarehouseVacantTasksViewRaw = props => {
   const [viewModel] = useState(() => new WarehouseVacantViewModel({ history: props.history }))
@@ -42,6 +43,12 @@ export const WarehouseVacantTasksViewRaw = props => {
 
   const getRowClassName = params =>
     params.row.originalData.operationType === TaskOperationType.RECEIVE && params.row.barcode && classNames.successRow
+
+  const isDisableDowloadButton =
+    !viewModel.selectedTasks.length ||
+    viewModel.selectedTasks.length > 1 ||
+    viewModel.getCurrentData().filter(el => viewModel.selectedTasks.includes(el.id))[0]?.originalData.operationType !==
+      TaskOperationType.RECEIVE
 
   return (
     <React.Fragment>
@@ -64,17 +71,14 @@ export const WarehouseVacantTasksViewRaw = props => {
           )}
 
           <Button
-            disabled={
-              !viewModel.selectedTasks.length ||
-              viewModel.selectedTasks.length > 1 ||
-              viewModel.getCurrentData().filter(el => viewModel.selectedTasks.includes(el.id))[0]?.originalData
-                .operationType !== TaskOperationType.RECEIVE
-            }
+            disabled={isDisableDowloadButton}
             className={classNames.pickupOrdersButton}
             onClick={viewModel.onClickReportBtn}
           >
             {t(TranslationKey['Download task file'])}
-            <img src="/assets/icons/download.svg" alt="download task file" />
+            <DownloadIcon
+              className={cx(classNames.downloadIcon, { [classNames.disabledDownloadIcon]: isDisableDowloadButton })}
+            />
           </Button>
         </div>
 
