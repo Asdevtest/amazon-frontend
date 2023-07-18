9095b066932ae6194481f54ce98b4c5a211feb56
oltree <o.melex@bk.ru>
2023-07-18 15:26:57 +0300

    6870-fix(freeOrdersTable):changed content display and column widths

--- src/ui/components/table/table-columns/buyer/buyer-fre-orders-columns/buyer-free-orders-columns.jsx
+++ src/ui/components/table/table-columns/buyer/buyer-fre-orders-columns/buyer-free-orders-columns.jsx
@@ -25,8 +25,8 @@ export const buyerFreeOrdersViewColumns = handlers => [
     headerName: t(TranslationKey.ID),
     renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.ID)} />,
 
-    width: 100,
-    renderCell: params => <MultilineTextCell text={params.value} />,
+    width: 65,
+    renderCell: params => <MultilineTextCell leftAlign text={params.value} />,
     type: 'number',
   },
 
@@ -34,7 +34,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     field: 'priorityAndChinaDelivery',
     headerName: t(TranslationKey.Priority),
     renderHeader: () => <IconHeaderCell url={'/assets/icons/bookmark.svg'} />,
-    width: 60,
+    width: 80,
     renderCell: params => (
       <PriorityAndChinaDeliverCell
         priority={params.row.originalData.priority}
@@ -44,6 +44,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     ),
     sortable: false,
     filterable: false,
+    align: 'center',
   },
 
   {
@@ -51,7 +52,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     headerName: t(TranslationKey.Action),
     renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.Action)} />,
 
-    width: 200,
+    width: 190,
     renderCell: params => (
       <NormalActionBtnCell
         tooltipText={t(TranslationKey['To assign the order to Byer'])}
@@ -69,7 +70,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     headerName: 'ASIN',
     renderHeader: () => <MultilineTextHeaderCell text={'ASIN'} />,
 
-    width: 400,
+    width: 390,
     renderCell: params => <OrderCell product={params.row.originalData.product} />,
   },
 
@@ -80,7 +81,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
 
     renderCell: params => <MultilineTextCell text={params.value} />,
     type: 'number',
-    width: 150,
+    width: 100,
   },
 
   {
@@ -90,7 +91,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
 
     renderCell: params => <MultilineTextCell text={toFixedWithDollarSign(params.row.originalData.totalPrice, 2)} />,
     type: 'number',
-    width: 90,
+    width: 110,
   },
 
   {
@@ -98,7 +99,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     headerName: t(TranslationKey.BarCode),
     renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.BarCode)} />,
 
-    width: 200,
+    width: 130,
     renderCell: params => (
       <DownloadAndCopyBtnsCell value={params.value} isFirstRow={params.api.getSortedRowIds()?.[0] === params.row.id} />
     ),
@@ -133,7 +134,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
         text={params.value ? formatDate(params.value) : ''}
       />
     ),
-    width: 200,
+    width: 115,
   },
 
   {
@@ -141,7 +142,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     headerName: t(TranslationKey['Re-search supplier']),
     renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Re-search supplier'])} />,
 
-    width: 100,
+    width: 120,
     renderCell: params => <MultilineTextCell text={params.value ? t(TranslationKey.Yes) : t(TranslationKey.No)} />,
   },
 
@@ -153,7 +154,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     renderCell: params => (
       <UserLinkCell blackText name={params.value} userId={params.row.originalData.storekeeper?._id} />
     ),
-    width: 200,
+    width: 170,
   },
 
   {
@@ -164,7 +165,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     renderCell: params => (
       <UserLinkCell blackText name={params.value} userId={params.row.originalData.product.client?._id} />
     ),
-    width: 200,
+    width: 160,
   },
 
   {
@@ -173,7 +174,7 @@ export const buyerFreeOrdersViewColumns = handlers => [
     renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.Destination)} />,
 
     renderCell: params => <MultilineTextCell text={params.value} />,
-    width: 200,
+    width: 160,
   },
 
   {
