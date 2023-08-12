3a90475b97392657f3acbc7017882893cb80e303
oltree <o.melex@bk.ru>
2023-08-12 19:17:54 +0300

    7105-fix(loadButton): fixed styles

--- src/ui/components/shared/upload-files-input/upload-files-input.style.js
+++ src/ui/components/shared/upload-files-input/upload-files-input.style.js
@@ -288,6 +288,13 @@ export const useClassNames = makeStyles()(theme => ({
     height: '40px',
     width: '172px',
     color: '#fff',
+    fontSize: 16,
+    lineHeight: '22px',
+
+    [theme.breakpoints.down(1024)]: {
+      fontSize: 14,
+      lineHeight: '19px',
+    },
   },
 
   errorText: {
