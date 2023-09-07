3b419be9b34b6d2176447e4adbe9a47e1495e012
oltree <o.melex@bk.ru>
2023-09-07 17:57:26 +0300

    7433-fix(select): fixed styles

--- src/ui/components/shops-integrations/shops-view/shops-view.style.js
+++ src/ui/components/shops-integrations/shops-view/shops-view.style.js
@@ -66,6 +66,6 @@ export const styles = theme => ({
     textOverflow: 'ellipsis',
     overflow: 'hidden',
     whiteSpace: 'nowrap',
-    color: theme.palette.primary.main,
+    color: theme.palette.text.general,
   },
 })
