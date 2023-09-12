7c1215b3547ebffbf8e32a811a3ce63fa2d7c22b
oltree <o.melex@bk.ru>
2023-09-12 16:23:01 +0300

    7478-fix(creatingNewBoxesModal): replaced the switcher to a new swither

--- src/ui/components/modals/edit-order-modal/edit-order-modal.style.js
+++ src/ui/components/modals/edit-order-modal/edit-order-modal.style.js
@@ -266,22 +266,22 @@ export const useClassNames = makeStyles()(theme => ({
 
   orange: {
     color: '#F3AF00 !important',
-    '-webkit-text-fill-color': '#F3AF00 !important',
+    WebkitTextFillColor: '#F3AF00 !important',
   },
 
   red: {
     color: `${theme.palette.orderStatus.red} !important`,
-    '-webkit-text-fill-color': `${theme.palette.orderStatus.red} !important`,
+    WebkitTextFillColor: `${theme.palette.orderStatus.red} !important`,
   },
 
   green: {
     color: `${theme.palette.text.green} !important`,
-    '-webkit-text-fill-color': `${theme.palette.text.green} !important`,
+    WebkitTextFillColor: `${theme.palette.text.green} !important`,
   },
 
   blue: {
     color: `${theme.palette.primary.main} !important`,
-    '-webkit-text-fill-color': `${theme.palette.primary.main} !important`,
+    WebkitTextFillColor: `${theme.palette.primary.main} !important`,
   },
 
   priorityWrapper: {
