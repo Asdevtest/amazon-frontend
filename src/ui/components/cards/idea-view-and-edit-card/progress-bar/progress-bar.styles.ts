f3369815b3aa18b31c2a0764f7ae1969c2d83f7f
oltree <o.melex@bk.ru>
2023-09-23 17:30:04 +0300

    7534-fix(productBlock): fixed styles

--- src/ui/components/cards/idea-view-and-edit-card/progress-bar/progress-bar.styles.ts
+++ src/ui/components/cards/idea-view-and-edit-card/progress-bar/progress-bar.styles.ts
@@ -21,7 +21,7 @@ export const useClassNames = makeStyles()(theme => ({
     display: 'flex',
     alignItems: 'center',
     height: '40px',
-    padding: '0px 10px 0px 13px',
+    padding: '0px 10px',
     position: 'relative',
   },
 
