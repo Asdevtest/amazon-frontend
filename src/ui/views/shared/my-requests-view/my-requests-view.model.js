8796afe784de1189020fa1f06d0a7d6bfcf14b80
oltree <oleg.melekh.frontend@gmail.com>
2023-10-26 13:39:13 +0300

    7853-fix(clientMyRequests): fixed pagination

--- src/ui/views/shared/my-requests-view/my-requests-view.model.js
+++ src/ui/views/shared/my-requests-view/my-requests-view.model.js
@@ -129,7 +129,7 @@ export class MyRequestsViewModel {
     () => this.onHover,
   )
 
-  paginationModel = { page: 0, pageSize: 100 }
+  paginationModel = { page: 0, pageSize: 15 }
   columnVisibilityModel = {}
 
   columnMenuSettings = {
