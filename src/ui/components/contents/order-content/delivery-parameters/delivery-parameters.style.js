3a7bf0172ed89976deb3fcbef75c3b546609b261
oltree <oleg.melekh.frontend@gmail.com>
2023-11-10 13:44:27 +0300

    8029-fix(deliveryParameters): fixed styles

--- src/ui/components/contents/order-content/delivery-parameters/delivery-parameters.style.js
+++ src/ui/components/contents/order-content/delivery-parameters/delivery-parameters.style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from 'tss-react/mui'
 
-export const useClassNames = makeStyles()(theme => ({
+export const useStyles = makeStyles()(theme => ({
   root: {
     width: '100%',
   },
@@ -15,6 +15,7 @@ export const useClassNames = makeStyles()(theme => ({
     alignItems: 'center',
     marginBottom: 20,
     transition: '.3s ease',
+
     '&:hover': {
       cursor: 'pointer',
       transform: 'scale(1.01)',
@@ -50,21 +51,6 @@ export const useClassNames = makeStyles()(theme => ({
     width: 160,
   },
 
-  text: {
-    color: theme.palette.text.general,
-    fontSize: '16px',
-    fontWeight: 600,
-    lineHeight: '19px',
-    maxWidth: '200px',
-    textAlign: 'right',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  },
-
-  storekeeperWrapper: {
-    marginTop: '40px',
-  },
-
   fieldLabel: {
     fontSize: '16px',
     fontWeight: 400,
@@ -77,22 +63,9 @@ export const useClassNames = makeStyles()(theme => ({
     marginTop: '40px',
   },
 
-  avatar: {
-    width: '28px',
-    height: '28px',
-  },
-
   intWarehouseWrapper: {
     display: 'flex',
     alignItems: 'center',
     gap: '5px',
-  },
-
-  chosenTariff: {
-    color: theme.palette.text.general,
-  },
-
-  notChosenTariff: {
-    color: '#fff',
   },
 }))
