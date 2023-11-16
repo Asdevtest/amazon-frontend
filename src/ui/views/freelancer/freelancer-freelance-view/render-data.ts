29dacce01bfbac06888b2aa577cb41af1742175d
Ulad <vladis67948@gmail.com>
2023-11-16 12:10:28 +0300

    8073-fix(platform): added buttons

---
+++ src/ui/views/freelancer/freelancer-freelance-view/render-data.ts
@@ -0,0 +1,18 @@
+export const renderData = [
+  {
+    link: '/freelancer/freelance/vacant-requests',
+    text: 'Vacant requests',
+  },
+  {
+    link: '/freelancer/freelance/my-proposals',
+    text: 'My proposals',
+  },
+  {
+    link: '/freelancer/freelance/my-services',
+    text: 'My services',
+  },
+  {
+    link: '/freelancer/freelance/source-files',
+    text: 'Source Files',
+  },
+]
