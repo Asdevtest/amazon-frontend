ebb571486a57e0479827d1c33cb9525e470ac69e
Ulad <vladis67948@gmail.com>
2023-11-17 15:59:03 +0300

    8160-fix(platform): bugs fixed

--- src/ui/components/forms/payment-methods-form/payment-methods-form.style.js
+++ src/ui/components/forms/payment-methods-form/payment-methods-form.style.js
@@ -2,7 +2,7 @@ import { makeStyles } from 'tss-react/mui'
 
 export const useStyles = makeStyles()(theme => ({
   root: {
-    maxWidth: 1685,
+    maxWidth: 1695,
     padding: 10,
     display: 'flex',
     flexDirection: 'column',
