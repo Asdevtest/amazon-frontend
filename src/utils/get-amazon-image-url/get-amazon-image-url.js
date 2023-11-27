93359afc34fdb4e6529230d13f87da7276a41958
oltree <oleg.melekh.frontend@gmail.com>
2023-11-27 11:34:17 +0300

    8258-fix(slider): fixed file icon

--- src/utils/get-amazon-image-url/get-amazon-image-url.js
+++ src/utils/get-amazon-image-url/get-amazon-image-url.js
@@ -26,7 +26,7 @@ export const getAmazonImageUrl = (str, isBig) => {
       ? !isBig && !str.includes('.preview.webp')
         ? str + amazonImageUrlPostfix
         : str
-      : str + amazonImageUrlPostfix
+      : str /* + amazonImageUrlPostfix */ // removed for google disk link or files
   } else if (str.includes('/uploads/')) {
     return `${BACKEND_API_URL}${str}${
       !checkIsGif(str) /* && !checkIsImageInludesPostfixes(str) */ &&
