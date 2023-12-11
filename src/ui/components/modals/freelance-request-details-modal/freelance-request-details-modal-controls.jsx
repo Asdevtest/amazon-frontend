cab8f50e152bd2908f6b04d7bdc5f55e263e5e15
oltree <oleg.melekh.frontend@gmail.com>
2023-12-11 09:52:51 +0300

    8289-fix(requestModal): added new conditions to the button display

--- src/ui/components/modals/freelance-request-details-modal/freelance-request-details-modal-controls.jsx
+++ src/ui/components/modals/freelance-request-details-modal/freelance-request-details-modal-controls.jsx
@@ -32,12 +32,28 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
   const { classes: styles, cx } = useStyles()
   const [isRestoreModalOpen, setIsRestoreModalOpen] = useState(false)
 
+  const showMarkAsCompletedButtton =
+    isAcceptedProposals &&
+    (request?.status !== RequestStatus.FORBID_NEW_PROPOSALS ||
+      request?.status !== RequestStatus.COMPLETE_PROPOSALS_AMOUNT_ACHIEVED)
+  const showChangeRequestTermsButton =
+    request?.status === RequestStatus.IN_PROCESS ||
+    request?.status === RequestStatus.EXPIRED ||
+    request?.status === RequestStatus.COMPLETE_PROPOSALS_AMOUNT_ACHIEVED
+  const showAcceptingProposalsButton =
+    request?.status !== RequestStatus.COMPLETE_PROPOSALS_AMOUNT_ACHIEVED &&
+    request?.status !== RequestStatus.EXPIRED &&
+    request?.status !== RequestStatus.DRAFT
+  const showMainActionsButton =
+    request && (request?.status === RequestStatus.DRAFT || request?.status === RequestStatus.PUBLISHED)
+  const showPublishButton = request?.status === RequestStatus.DRAFT
+
   return (
     <div className={styles.suggestDeal}>
       <OpenInNewTab onClickOpenNewTab={() => onClickOpenNewTab(request?._id)} />
 
       <div className={styles.controlsWrapper}>
-        {isAcceptedProposals && (
+        {showMarkAsCompletedButtton && (
           <Button success className={styles.publishBtn} onClick={() => onClickMarkAsCompletedBtn(request?._id)}>
             {t(TranslationKey['Mark as completed'])}
           </Button>
@@ -51,9 +67,10 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
               onClick={() => onToggleUploadedToListing(request?._id, request?.uploadedToListing)}
             >
               <Checkbox color="primary" checked={request?.uploadedToListing} className={styles.listingCheckbox} />
-              <Typography className={cx(styles.listingText)}>{t(TranslationKey['Uploaded by on listing'])}</Typography>
+              <Typography className={styles.listingText}>{t(TranslationKey['Uploaded by on listing'])}</Typography>
             </Button>
-            {request && (request?.status === RequestStatus.DRAFT || request?.status === RequestStatus.PUBLISHED) && (
+
+            {showMainActionsButton && (
               <>
                 <Button
                   danger
@@ -73,7 +90,7 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
                   {t(TranslationKey.Edit)}
                 </Button>
 
-                {request?.status === RequestStatus.DRAFT && (
+                {showPublishButton && (
                   <Button
                     success
                     tooltipInfoContent={t(TranslationKey['Publish the selected request on the exchange'])}
@@ -86,9 +103,7 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
               </>
             )}
 
-            {(request?.status === RequestStatus.IN_PROCESS ||
-              request?.status === RequestStatus.EXPIRED ||
-              request?.status === RequestStatus.COMPLETE_PROPOSALS_AMOUNT_ACHIEVED) && (
+            {showChangeRequestTermsButton && (
               <>
                 <Button className={styles.recoverBtn} onClick={() => setIsRestoreModalOpen(true)}>
                   {t(TranslationKey['Change request terms'])}
@@ -97,7 +112,7 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
                 <Modal openModal={isRestoreModalOpen} setOpenModal={() => setIsRestoreModalOpen(false)}>
                   <RestoreRequestModal
                     currentDate={request?.timeoutAt}
-                    currentRequestsCount={request.maxAmountOfProposals}
+                    currentRequestsCount={request?.maxAmountOfProposals}
                     handleCloseModal={() => setIsRestoreModalOpen(false)}
                     handleSubmit={onRecoverRequest}
                   />
@@ -105,25 +120,23 @@ export const FreelanceRequestDetailsModalControls = memo(props => {
               </>
             )}
 
-            {request?.status !== RequestStatus.COMPLETE_PROPOSALS_AMOUNT_ACHIEVED &&
-              request?.status !== RequestStatus.EXPIRED &&
-              request?.status !== RequestStatus.DRAFT && (
-                <Button
-                  tooltipInfoContent={
-                    request?.status !== RequestStatus.FORBID_NEW_PROPOSALS &&
-                    t(TranslationKey['Removes the visibility of the request on the exchange'])
-                  }
-                  color="primary"
-                  className={cx({
-                    [styles.stopBtn]: request?.status !== RequestStatus.FORBID_NEW_PROPOSALS,
-                  })}
-                  onClick={request?.status !== 'FORBID_NEW_PROPOSALS' ? onClickAbortBtn : onClickPublishBtn}
-                >
-                  {request?.status === RequestStatus.FORBID_NEW_PROPOSALS
-                    ? t(TranslationKey['Resume accepting proposals'])
-                    : t(TranslationKey['Stop accepting proposals'])}
-                </Button>
-              )}
+            {showAcceptingProposalsButton && (
+              <Button
+                tooltipInfoContent={
+                  request?.status !== RequestStatus.FORBID_NEW_PROPOSALS &&
+                  t(TranslationKey['Removes the visibility of the request on the exchange'])
+                }
+                color="primary"
+                className={cx({
+                  [styles.stopBtn]: request?.status !== RequestStatus.FORBID_NEW_PROPOSALS,
+                })}
+                onClick={request?.status !== RequestStatus.FORBID_NEW_PROPOSALS ? onClickAbortBtn : onClickPublishBtn}
+              >
+                {request?.status === RequestStatus.FORBID_NEW_PROPOSALS
+                  ? t(TranslationKey['Resume accepting proposals'])
+                  : t(TranslationKey['Stop accepting proposals'])}
+              </Button>
+            )}
           </>
         )}
 
