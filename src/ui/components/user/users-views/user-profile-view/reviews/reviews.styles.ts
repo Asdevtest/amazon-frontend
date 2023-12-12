5b1d18cf7616265d873724ae4c76a5835da94986
oltree <oleg.melekh.frontend@gmail.com>
2023-12-12 10:39:10 +0300

    8332-feat(rewiews): changed styles and added text editor

--- src/ui/components/user/users-views/user-profile-view/reviews/reviews.styles.ts
+++ src/ui/components/user/users-views/user-profile-view/reviews/reviews.styles.ts
@@ -1,32 +1,41 @@
 import { makeStyles } from 'tss-react/mui'
 
-export const useReviewsStyles = makeStyles()(theme => ({
-  mainTitle: {
-    fontSize: '18px',
-    fontWeight: '600',
-    color: theme.palette.text.general,
-    paddingBottom: '10px',
-  },
-
-  typoNoReviews: {
-    textAlign: 'center',
-  },
-
-  mainWrapper: {
+export const useStyles = makeStyles()(theme => ({
+  wrapper: {
     width: '100%',
   },
 
-  body: {
-    border: '1px solid #e0e0e0',
+  title: {
+    fontSize: 18,
+    lineHeight: '25px',
+    fontWeight: 600,
+    color: theme.palette.text.general,
   },
 
-  reviewList: {
-    padding: '5px 5px',
-    margin: '10px 5px',
+  titleCenter: {
+    height: '100%',
     display: 'flex',
-    flexDirection: 'column',
-    gap: '20px',
-    maxHeight: '632px',
-    overflow: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  body: {
+    padding: '10px 0',
+    marginTop: 10,
+    border: '1px solid #e0e0e0',
+    boxShadow: theme.palette.boxShadow.paper,
+    borderRadius: 10,
+  },
+
+  scroll_Y: {
+    padding: '0 20px',
+    height: '68vh',
+    overflowY: 'auto',
+  },
+
+  lastReview: {
+    'div:last-child': {
+      borderBottom: 'none',
+    },
   },
 }))
