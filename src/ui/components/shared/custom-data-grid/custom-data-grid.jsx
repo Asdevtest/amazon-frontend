9ff18a699018c6fa05ebbd9531544eb8464f21b9
Ulad <vladis67948@gmail.com>
2023-12-12 12:20:47 +0300

    8358-fix(dataGrid): data grid default version

--- src/ui/components/shared/custom-data-grid/custom-data-grid.jsx
+++ src/ui/components/shared/custom-data-grid/custom-data-grid.jsx
@@ -1,34 +1,18 @@
-import { useEffect } from 'react'
-
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined'
-import { DataGrid, useGridApiRef } from '@mui/x-data-grid'
+import { DataGrid } from '@mui/x-data-grid'
 
 import { SettingsModel } from '@models/settings-model'
 
 import { DataGridCustomColumnMenuComponent } from '@components/data-grid/data-grid-custom-components/data-grid-custom-column-component'
 import { DataGridCustomToolbar } from '@components/data-grid/data-grid-custom-components/data-grid-custom-toolbar'
 
-export const CustomDataGrid = ({ rows, ...restProps }) => {
-  const apiRef = useGridApiRef()
-
-  // console.log(apiRef)
-  // console.log('rows', rows)
-
-  // useEffect(() => {
-  //   if (!apiRef.current || !rows.length) {
-  //     return
-  //   }
-  //   apiRef.current.updateRows(() => {
-  //     return rows
-  //   })
-  // }, [SettingsModel.languageTag])
-
+export const CustomDataGrid = ({ ...restProps }) => {
   return (
     <DataGrid
+      key={SettingsModel.languageTag}
       pagination
       hideFooter
       disableVirtualization
-      apiRef={apiRef}
       sortingMode="server"
       paginationMode="server"
       pageSizeOptions={[15, 25, 50, 100]}
@@ -37,7 +21,6 @@ export const CustomDataGrid = ({ rows, ...restProps }) => {
         columnMenuIcon: FilterAltOutlinedIcon,
         columnMenu: DataGridCustomColumnMenuComponent,
       }}
-      rows={rows}
       {...restProps}
     />
   )
