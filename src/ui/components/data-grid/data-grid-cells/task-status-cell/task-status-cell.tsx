e46c79a088440879721e829201fd39db22c82f89
VladosG155-prog <vladosk2004@gmail.com>
2023-12-28 22:00:56 +0300

    fix: status has been showing

--- src/ui/components/data-grid/data-grid-cells/task-status-cell/task-status-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/task-status-cell/task-status-cell.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import React, { FC } from 'react'
 
-import { TaskStatus, TaskStatusTranslate, mapTaskStatusKeyToEnum } from '@constants/task/task-status'
+import { TaskStatus, TaskStatusTranslate } from '@constants/task/task-status'
 
 import { useStyles } from './task-status-cell.style'
 
@@ -13,14 +13,13 @@ export const TaskStatusCell: FC<TaskStatusCellProps> = React.memo(({ status }) =
   const { classes: styles } = useStyles()
 
   // @ts-ignore
-  const validStatus = mapTaskStatusKeyToEnum[status]
 
   const colorByStatus = () => {
-    if ([TaskStatus.AT_PROCESS, TaskStatus.NEW].includes(validStatus)) {
+    if ([TaskStatus.AT_PROCESS, TaskStatus.NEW].includes(status)) {
       return '#F3AF00'
-    } else if ([TaskStatus.SOLVED].includes(validStatus)) {
+    } else if ([TaskStatus.SOLVED].includes(status)) {
       return '#00B746'
-    } else if ([TaskStatus.NOT_SOLVED].includes(validStatus)) {
+    } else if ([TaskStatus.NOT_SOLVED].includes(status)) {
       return '#FF1616'
     } else {
       return '#black'
@@ -32,7 +31,7 @@ export const TaskStatusCell: FC<TaskStatusCellProps> = React.memo(({ status }) =
   return (
     <div className={styles.statusWrapper}>
       <p className={styles.orderStatusText} style={{ color: colorStatus }}>
-        {TaskStatusTranslate(validStatus)}
+        {TaskStatusTranslate(status)}
       </p>
     </div>
   )
