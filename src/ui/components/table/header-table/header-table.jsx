7637a1bbb1a5854753910f959c60dee104462c47
VladosG155-prog <vladosk2004@gmail.com>
2023-12-29 20:27:06 +0300

    fix: added header-table component

---
+++ src/ui/components/table/header-table/header-table.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+import { TranslationKey } from '@constants/translations/translation-key'
+
+import { Button } from '@components/shared/buttons/button'
+import { CustomSwitcher } from '@components/shared/custom-switcher'
+import { SearchInput } from '@components/shared/search-input'
+
+import { t } from '@utils/translations'
+
+import { useStyles } from './header-table.style'
+
+export const HeaderTable = ({ viewModel }) => {
+  const { classes: styles } = useStyles()
+
+  const switcherSettings = [
+    ...viewModel.storekeepersData
+      .slice()
+      .filter(storekeeper => storekeeper.boxesCount !== 0)
+      .sort((a, b) => a.name?.localeCompare(b.name))
+      .map(storekeeper => ({ label: () => storekeeper.name, value: storekeeper._id })),
+    { label: () => t(TranslationKey['All warehouses']), value: undefined },
+  ]
+
+  return (
+    <React.Fragment>
+      <div className={styles.searchWrapper}>
+        <SearchInput
+          key={'client_batches_awaiting-batch_search_input'}
+          inputClasses={styles.searchInput}
+          value={viewModel.nameSearchValue}
+          placeholder={t(TranslationKey['Search by ASIN, Title, Batch ID, Order ID'])}
+          onSubmit={viewModel.onSearchSubmit}
+        />
+      </div>
+      <div className={styles.btnsWrapper}>
+        <div className={styles.btnsSubWrapper}>
+          <Button
+            disabled={!viewModel.selectedBatches.length}
+            tooltipInfoContent={t(
+              TranslationKey['Returns all boxes from the selected batch to the "Boxes ready to send" section'],
+            )}
+            styles={styles.cancelBtn}
+            color="primary"
+            variant="contained"
+            onClick={() => viewModel.onTriggerOpenModal('showConfirmModal')}
+          >
+            {t(TranslationKey['Cancel Send'])}
+          </Button>
+
+          <CustomSwitcher
+            switchMode={'medium'}
+            condition={viewModel.currentStorekeeperId}
+            switcherSettings={switcherSettings}
+            changeConditionHandler={viewModel.onClickStorekeeperBtn}
+          />
+        </div>
+
+        <div className={styles.rightSideButtonsWrapper}>
+          <Button
+            disabled={viewModel.selectedBatches.length !== 1}
+            variant="contained"
+            styles={styles.rightSideButton}
+            onClick={() => viewModel.onClickAddOrEditBatch({ isAdding: false })}
+          >
+            {t(TranslationKey['Edit batch'])}
+          </Button>
+          <Button
+            success
+            styles={styles.rightSideButton}
+            variant="contained"
+            onClick={() => viewModel.onClickAddOrEditBatch({ isAdding: true })}
+          >
+            {t(TranslationKey['Create a batch'])}
+          </Button>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
