f4275b76239f8a1ef23cd1ac1653358239ea5934
VladosG155-prog <vladosk2004@gmail.com>
2024-01-11 14:19:38 +0300

    deleted unused fragment

--- src/ui/components/table/table-columns/client/client-inventory-columns/client-inventory-columns.jsx
+++ src/ui/components/table/table-columns/client/client-inventory-columns/client-inventory-columns.jsx
@@ -473,12 +473,10 @@ export const clientInventoryColumns = (
       />
     ),
     renderCell: params => (
-      <>
-        <MultilineTextCell
-          text={productStatusTranslateKey(ProductStatusByCode[params.row.originalData.status])}
-          color={colorByProductStatus(ProductStatusByCode[params.row.originalData.status])}
-        />
-      </>
+      <MultilineTextCell
+        text={productStatusTranslateKey(ProductStatusByCode[params.row.originalData.status])}
+        color={colorByProductStatus(ProductStatusByCode[params.row.originalData.status])}
+      />
     ),
     width: 100,
 
