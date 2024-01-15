125e2539fe9b7084bde61491050591f0e6e817e2
Ulad <vladis67948@gmail.com>
2024-01-15 10:02:55 +0300

    merge: up to v4.0.2030

--- src/ui/components/shared/custom-data-grid/custom-data-grid.jsx
+++ src/ui/components/shared/custom-data-grid/custom-data-grid.jsx
@@ -1,5 +1,6 @@
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined'
-import { DataGrid } from '@mui/x-data-grid'
+// import { DataGrid } from '@mui/x-data-grid'
+import { DataGridPremium } from '@mui/x-data-grid-premium'
 
 import { SettingsModel } from '@models/settings-model'
 
@@ -8,31 +9,21 @@ import { DataGridCustomToolbar } from '@components/data-grid/data-grid-custom-co
 
 import { getLocalizationByLanguageTag } from '@utils/data-grid-localization'
 
-export const CustomDataGrid = ({ ...restProps }) => {
-  const slots =
-    restProps.paginationMode === 'client'
-      ? {
-          toolbar: DataGridCustomToolbar,
-          columnMenuIcon: FilterAltOutlinedIcon,
-        }
-      : {
-          toolbar: DataGridCustomToolbar,
-          columnMenuIcon: FilterAltOutlinedIcon,
-          columnMenu: DataGridCustomColumnMenuComponent,
-        }
-
-  return (
-    <DataGrid
-      key={SettingsModel.languageTag}
-      pagination
-      hideFooter
-      disableVirtualization
-      sortingMode="server"
-      paginationMode="server"
-      pageSizeOptions={[15, 25, 50, 100]}
-      localeText={getLocalizationByLanguageTag()}
-      slots={slots}
-      {...restProps}
-    />
-  )
-}
+export const CustomDataGrid = ({ ...restProps }) => (
+  <DataGridPremium
+    key={SettingsModel.languageTag}
+    pagination
+    hideFooter
+    disableVirtualization
+    sortingMode="server"
+    paginationMode="server"
+    pageSizeOptions={[15, 25, 50, 100]}
+    localeText={getLocalizationByLanguageTag()}
+    slots={{
+      toolbar: DataGridCustomToolbar,
+      columnMenuIcon: FilterAltOutlinedIcon,
+      columnMenu: restProps.paginationMode === 'client' ? DataGridCustomColumnMenuComponent : null,
+    }}
+    {...restProps}
+  />
+)
