1797fb1fc54cc9020fe7c34215c781d5abd9d8f8
Ulad <vladis67948@gmail.com>
2024-02-13 11:54:15 +0300

    fix(inventory): filter fix

--- src/ui/views/client/client-inventory-view/client-inventory-view.model.js
+++ src/ui/views/client/client-inventory-view/client-inventory-view.model.js
@@ -230,7 +230,7 @@ export class ClientInventoryViewModel extends DataGridFilterTableModel {
     const additionalPropertiesGetFilters = () => {
       const isNeedPurchaseFilter = this.columnMenuSettings.isNeedPurchaseFilterData.isNeedPurchaseFilter
       const isNotNeedPurchaseFilter = this.columnMenuSettings.isNeedPurchaseFilterData.isNotNeedPurchaseFilter
-      const purchaseQuantityAboveZero = isNeedPurchaseFilter && isNotNeedPurchaseFilter ? null : isNeedPurchaseFilter
+      const purchaseQuantityAboveZero = isNeedPurchaseFilter && isNotNeedPurchaseFilter ? false : true
 
       return {
         ...(this.columnMenuSettings.isHaveBarCodeFilterData.isHaveBarCodeFilter !== null && {
@@ -247,7 +247,7 @@ export class ClientInventoryViewModel extends DataGridFilterTableModel {
             }),
 
         ...(purchaseQuantityAboveZero && {
-          purchaseQuantityAboveZero: { $eq: true },
+          purchaseQuantityAboveZero: { $eq: isNeedPurchaseFilter },
         }),
 
         ...(this.isArchive && {
