845481d68ac9eb3345c7a2a80f459a35f09ad3eb
oltree <oleg.melekh.frontend@gmail.com>
2024-03-26 14:12:00 +0300

    9288-fix(fileLogic): remove log

--- src/ui/components/modals/main-request-result-modal/components/tabs/components/files-tab/files-tab.tsx
+++ src/ui/components/modals/main-request-result-modal/components/tabs/components/files-tab/files-tab.tsx
@@ -65,11 +65,6 @@ export const FilesTab: FC<FilesTabProps> = memo(props => {
 
   const lastFileRef = useScrollToFile(files.length)
 
-  console.log(
-    'files',
-    files.map(file => file.index),
-  )
-
   return (
     <>
       <div className={styles.wrapper}>
