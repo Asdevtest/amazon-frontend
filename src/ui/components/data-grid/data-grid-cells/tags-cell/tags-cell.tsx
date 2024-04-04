56b854faa5bf8a8f632dd17d5d7c42a15bf02c5f
Ulad <vladis67948@gmail.com>
2024-04-04 10:22:59 +0300

    9264-feat(tags_cell): added edit button

--- src/ui/components/data-grid/data-grid-cells/tags-cell/tags-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/tags-cell/tags-cell.tsx
@@ -3,35 +3,47 @@ import { FC, memo } from 'react'
 
 import { Tooltip } from '@mui/material'
 
+import { Button } from '@components/shared/button'
+import { EditIcon } from '@components/shared/svg-icons'
+
 import { IItemWithTitle } from '@hooks/use-select'
 
 import { useStyles } from './tags-cell.style'
 
 interface TagsCellProps {
   tags: IItemWithTitle[]
-  onClickTag: (tag: IItemWithTitle) => void
+  onClickTag?: (tag: IItemWithTitle) => void
+  onClickEdit?: () => void
 }
 
-export const TagsCell: FC<TagsCellProps> = memo(({ tags, onClickTag }) => {
+export const TagsCell: FC<TagsCellProps> = memo(({ tags, onClickTag, onClickEdit }) => {
   const { classes: styles, cx } = useStyles()
 
   return (
-    <div className={styles.tags}>
-      {tags?.map((el, index: number) => {
-        const createTagText = `#${el.title}`
+    <div className={cx(styles.tagsWrapper, { [styles.editMode]: !!onClickEdit })}>
+      <div className={styles.tags}>
+        {tags?.map((el, index: number) => {
+          const createTagText = `#${el.title}`
 
-        return (
-          <Tooltip key={el._id} placement="top" title={createTagText}>
-            <button
-              className={cx(styles.tagItem, { [styles.activeButton]: !!onClickTag })}
-              onClick={() => onClickTag?.(el)}
-            >
-              {createTagText}
-              {index !== tags.length - 1 && ', '}
-            </button>
-          </Tooltip>
-        )
-      })}
+          return (
+            <Tooltip key={el._id} placement="top" title={createTagText}>
+              <button
+                className={cx(styles.tagItem, { [styles.activeButton]: !!onClickTag })}
+                onClick={() => onClickTag?.(el)}
+              >
+                {createTagText}
+                {index !== tags.length - 1 && ', '}
+              </button>
+            </Tooltip>
+          )
+        })}
+      </div>
+
+      {onClickEdit ? (
+        <Button iconButton>
+          <EditIcon />
+        </Button>
+      ) : null}
     </div>
   )
 })
