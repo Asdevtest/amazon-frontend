62a20376594683c72b428fffc9dfd13fff832cba
oltree <oleg.melekh.frontend@gmail.com>
2024-04-05 09:53:28 +0300

    9344-fix(actionButtons): fix styles

--- src/ui/components/shared/selects/select/select.tsx
+++ src/ui/components/shared/selects/select/select.tsx
@@ -72,7 +72,7 @@ export const Select: FC<SelectProps> = memo(props => {
           </button>
 
           {filteredItems.map((item, index) => (
-            <button key={index} className={styles.menuItem} onClick={() => handleChangeSelectedItem(item._id)}>
+            <div key={index} className={styles.menuItem} onClick={() => handleChangeSelectedItem(item._id)}>
               <p className={styles.menuItemText}>{item.name}</p>
               {withFaworites && (
                 <button
@@ -89,7 +89,7 @@ export const Select: FC<SelectProps> = memo(props => {
                   />
                 </button>
               )}
-            </button>
+            </div>
           ))}
         </ul>
       </div>
