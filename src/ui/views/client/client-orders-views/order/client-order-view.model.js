2be261a992c213bcea57d829d6e4a00e6c41aa9e
oltree <oleg.melekh.frontend@gmail.com>
2024-04-09 14:43:49 +0300

    9399-fix(toast): add toast success

--- src/ui/views/client/client-orders-views/order/client-order-view.model.js
+++ src/ui/views/client/client-orders-views/order/client-order-view.model.js
@@ -1,4 +1,5 @@
 import { makeAutoObservable, runInAction } from 'mobx'
+import { toast } from 'react-toastify'
 
 import { TranslationKey } from '@constants/translations/translation-key'
 
@@ -266,9 +267,9 @@ export class ClientOrderViewModel {
 
       await this.getOrderById()
 
-      this.onTriggerOpenModal('showWarningInfoModal')
-
       this.onTriggerOpenModal('showConfirmModal')
+
+      toast.success(t(TranslationKey['Order successfully created!']))
     } catch (error) {
       console.error(error)
     }
