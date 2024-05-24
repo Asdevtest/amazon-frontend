636c97cc44df4147a741bb3ea087e33193db9bdd
Ulad <vladis67948@gmail.com>
2024-05-24 08:12:26 +0300

    merge: up to v4.11.0420

--- src/ui/components/forms/add-or-edit-user-permissions-form/access-to-product-form/access-to-product-form.jsx
+++ src/ui/components/forms/add-or-edit-user-permissions-form/access-to-product-form/access-to-product-form.jsx
@@ -97,17 +97,12 @@ export const AccessToProductForm = memo(props => {
         chosenGoods?.length === allProductsIds?.length &&
         shop._id !== 'PRODUCTS_WITHOUT_SHOPS_ID'
 
-      console.log('chosenGoods :>> ', chosenGoods)
-      console.log('allProductsIds :>> ', allProductsIds)
-      console.log('=== :>> ', chosenGoods?.length === allProductsIds?.length)
-
       if (selectedAccess === accessProductSettings.NEED_SELECT) {
         return newDataToRender.map(item =>
           item._id === shop._id ? { ...item, tmpSelectedShop, tmpProductsIds: chosenGoods } : item,
         )
       } else if (selectedAccess === accessProductSettings.ALL_PRODUCTS) {
         setChooseAllCheck(true)
-        console.log('2')
 
         return newDataToRender.map(item =>
           item._id === shop._id
