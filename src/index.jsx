897a05caea57cafa3e94b5bcea7c72255e4812c6
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-04 09:31:46 +0300

    feat(sentry): add lib

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,4 @@
 import * as Sentry from '@sentry/react'
-import { BrowserTracing } from '@sentry/tracing'
-// import {DndProvider} from 'react-dnd'
-// import {HTML5Backend} from 'react-dnd-html5-backend'
 import ReactDOM from 'react-dom/client'
 import FaviconNotificationContextProvider from 'react-favicon-notification'
 import 'reflect-metadata'
@@ -13,19 +10,18 @@ import { reportWebVitals } from '@utils/report-web-vitals'
 import { App } from './app'
 
 Sentry.init({
-  integrations: [new BrowserTracing()],
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
 root.render(
   <FaviconNotificationContextProvider>
-    {/* <DndProvider backend={HTML5Backend}> */}
     <App />
-    {/* </DndProvider> */}
   </FaviconNotificationContextProvider>,
 )
 
