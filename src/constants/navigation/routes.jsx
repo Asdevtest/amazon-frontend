a0e145efec2f911f1050f1cbabe42e97110599d9
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-05 12:12:11 +0300

    9637-feat(reports): add view to routes and navbar

--- src/constants/navigation/routes.jsx
+++ src/constants/navigation/routes.jsx
@@ -181,6 +181,7 @@ const ClientFreelanceView = lazy(() =>
 const ClientInventoryView = lazy(() =>
   import('@views/client/client-inventory-view').then(module => ({ default: module.ClientInventoryView })),
 )
+const ReportsView = lazy(() => import('@views/shared/reports-view').then(module => ({ default: module.ReportsView })))
 const ClientBoxesNotificationsView = lazy(() =>
   import('@views/client/client-notifications-views/client-boxes-notifications-view').then(module => ({
     default: module.ClientBoxesNotificationsView,
@@ -1385,37 +1386,33 @@ export const privateRoutesConfigs = [
     },
   },
 
-  // ////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // {
-  //   routePath: '/client/inventory/archive',
-  //   component: ClientInventoryView,
-  //   exact: true,
-  //   permission: [UserRole.CLIENT],
-  //   permissionKey: permissionsKeys.client.SHOW_INVENTORY_CLIENT,
-
-  //   crumbNameKey: TranslationKey.Archive,
-
-  //   navigationInfo: {
-  //     activeCategory: navBarActiveCategory.NAVBAR_INVENTORY,
-  //     activeSubCategory: '',
-  //     title: () => t(TranslationKey.Inventory),
-  //   },
-  // },
-
   {
     routePath: '/client/inventory',
     component: ClientInventoryView,
     exact: true,
     permission: [UserRole.CLIENT],
     permissionKey: permissionsKeys.client.SHOW_INVENTORY_CLIENT,
-
     crumbNameKey: TranslationKey.Inventory,
 
     navigationInfo: {
       activeCategory: navBarActiveCategory.NAVBAR_INVENTORY,
-      activeSubCategory: '',
+      activeSubCategory: navBarActiveSubCategory.SUB_NAVBAR_CLIENT_INVENTORY,
       title: () => t(TranslationKey.Inventory),
+    },
+  },
+
+  {
+    routePath: '/client/inventory/reports',
+    component: ReportsView,
+    exact: true,
+    permission: [UserRole.CLIENT],
+    permissionKey: permissionsKeys.client.SHOW_REPORTS_CLIENT,
+    crumbNameKey: TranslationKey.Reports,
+
+    navigationInfo: {
+      activeCategory: navBarActiveCategory.NAVBAR_INVENTORY,
+      activeSubCategory: navBarActiveSubCategory.SUB_NAVBAR_CLIENT_INVENTORY_REPORTS,
+      title: () => t(TranslationKey.Reports),
     },
   },
 
