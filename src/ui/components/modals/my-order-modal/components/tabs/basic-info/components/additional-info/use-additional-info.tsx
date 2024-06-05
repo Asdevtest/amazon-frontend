3b62d9a8a773b9fee5fa138545a41be36d4ceb1a
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-05 09:27:53 +0300

    9838-fix(select): fix styles

--- src/ui/components/modals/my-order-modal/components/tabs/basic-info/components/additional-info/use-additional-info.tsx
+++ src/ui/components/modals/my-order-modal/components/tabs/basic-info/components/additional-info/use-additional-info.tsx
@@ -119,7 +119,7 @@ export const useAdditionalInfo = ({
               <p className={styles.tafiffText}>{currentTariffRate}</p>
             </>
           ) : (
-            <p>{t(TranslationKey.Select)}</p>
+            <p className={styles.tafiffText}>{t(TranslationKey.Select)}</p>
           )}
         </button>
       ),
