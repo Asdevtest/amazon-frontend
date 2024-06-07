efced2cd6de915950af3eb02424a6c12c2c38c4a
Temirlan Termechikov <temirlan.termechikov@gmail.com>
2024-06-07 15:38:25 +0600

    9807-feat(admin_user_table): refactored a className definition

--- src/ui/components/data-grid/data-grid-cells/user-mini-cell/user-mini-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/user-mini-cell/user-mini-cell.tsx
@@ -20,7 +20,7 @@ export const UserMiniCell: FC<UserMiniCellProps> = memo(props => {
   const { classes: styles, cx } = useStyles()
 
   return (
-    <div className={cx(styles.wrapper, wrapperClassName, isOnline && styles.onlineIcon)}>
+    <div className={cx(styles.wrapper, wrapperClassName, { [styles.onlineIcon]: isOnline })}>
       {userId && (
         <img src={getUserAvatarSrc(userId)} className={cx(styles.avatar, avatarClassName)} alt={`avatar-${userId}`} />
       )}
