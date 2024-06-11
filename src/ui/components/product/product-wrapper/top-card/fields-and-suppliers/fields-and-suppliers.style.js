6c429bd0a0dd4918b77ec664d2c9a556aa26241f
Temirlan Termechikov <temirlan.termechikov@gmail.com>
2024-06-11 16:38:02 +0600

    9912-fix(fieldsAndSuppliers): fixed parse product data click button behavior

--- src/ui/components/product/product-wrapper/top-card/fields-and-suppliers/fields-and-suppliers.style.js
+++ src/ui/components/product/product-wrapper/top-card/fields-and-suppliers/fields-and-suppliers.style.js
@@ -45,6 +45,10 @@ export const useStyles = makeStyles()(theme => ({
     width: 'calc(100% - 110px)',
   },
 
+  error: {
+    borderColor: 'red',
+  },
+
   text: {
     color: theme.palette.text.general,
     fontSize: '14px',
