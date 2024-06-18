1cc6a3e9e5f1ee137d435c29230e962518fee9f0
Temirlan Termechikov <temirlan.termechikov@gmail.com>
2024-06-18 12:12:19 +0600

    9979-fix(productVariationsCell): returned the Tooltip component and added a new prop

--- src/ui/components/data-grid/data-grid-cells/product-variations-cell/product-variations-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/product-variations-cell/product-variations-cell.tsx
@@ -1,6 +1,12 @@
 import { FC, memo } from 'react'
 
+import { Tooltip } from '@mui/material'
+
+import { TranslationKey } from '@constants/translations/translation-key'
+
 import { ParentProductIcon, VariationProductIcon } from '@components/shared/svg-icons'
+
+import { t } from '@utils/translations'
 
 import { useStyles } from './product-variations-cell.style'
 
@@ -8,28 +14,44 @@ interface ProductVariationsCellProps {
   showVariationButton: boolean
   isParentProduct: boolean
   onClickVariationButton: () => void
+  isTooltipVisible?: boolean
 }
 
 export const ProductVariationsCell: FC<ProductVariationsCellProps> = memo(
-  ({ showVariationButton, isParentProduct, onClickVariationButton }) => {
+  ({ showVariationButton, isParentProduct, onClickVariationButton, isTooltipVisible = true }) => {
     const { classes: styles } = useStyles()
+
+    const renderButton = () => (
+      <button
+        className={styles.iconWrapper}
+        onClick={e => {
+          e.stopPropagation()
+          onClickVariationButton()
+        }}
+      >
+        {isParentProduct ? (
+          <ParentProductIcon className={styles.shareLinkIcon} />
+        ) : (
+          <VariationProductIcon className={styles.shareLinkIcon} />
+        )}
+      </button>
+    )
 
     return (
       <div className={styles.productVariationsCellWrapper}>
         {showVariationButton ? (
-          <button
-            className={styles.iconWrapper}
-            onClick={e => {
-              e.stopPropagation()
-              onClickVariationButton()
-            }}
-          >
-            {isParentProduct ? (
-              <ParentProductIcon className={styles.shareLinkIcon} />
-            ) : (
-              <VariationProductIcon className={styles.shareLinkIcon} />
-            )}
-          </button>
+          isTooltipVisible ? (
+            <Tooltip
+              arrow
+              title={t(TranslationKey['Product variations'])}
+              placement="top"
+              classes={{ tooltip: styles.tooltip, arrow: styles.arrow }}
+            >
+              {renderButton()}
+            </Tooltip>
+          ) : (
+            renderButton()
+          )
         ) : null}
       </div>
     )
