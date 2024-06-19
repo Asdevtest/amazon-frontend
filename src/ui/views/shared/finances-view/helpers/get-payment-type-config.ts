71de9e55a241772a1a0e98aa3d5ea4a4c2adebd5
Ulad <vladis67948@gmail.com>
2024-06-19 11:06:39 +0300

    fix(finances): removed zero

--- src/ui/views/shared/finances-view/helpers/get-payment-type-config.ts
+++ src/ui/views/shared/finances-view/helpers/get-payment-type-config.ts
@@ -1,21 +1,18 @@
-import { getPaymentTypeTranslations } from '@constants/finances/get-type-translations'
-import { PaymentType } from '@constants/finances/payment-type'
 import { TranslationKey } from '@constants/translations/translation-key'
 
 import { t } from '@utils/translations'
 
-export const getPaymentTypeConfig = () => {
-  const defaultConfig = [
-    {
-      label: () => t(TranslationKey.All),
-      value: '',
-    },
-  ]
-
-  const options = Object.values(PaymentType).map(item => ({
-    label: () => getPaymentTypeTranslations(item),
-    value: item,
-  }))
-
-  return defaultConfig.concat(options)
-}
+export const getPaymentTypeConfig = () => [
+  {
+    label: () => t(TranslationKey.All),
+    value: '',
+  },
+  {
+    label: () => t(TranslationKey.Replenish),
+    value: 'REPLENISH',
+  },
+  {
+    label: () => t(TranslationKey.Fine),
+    value: 'FINE',
+  },
+]
