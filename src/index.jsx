bb3a16cb44b0de07a6a2e010134e0759d64bd10d
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-25 15:04:37 +0300

    sentry-test

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import * as Sentry from '@sentry/react'
-import { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import FaviconNotificationContextProvider from 'react-favicon-notification'
-import { useLocation } from 'react-router-dom'
 import 'reflect-metadata'
 
 import '@services/mobx-persist-configure'
@@ -13,18 +11,19 @@ import { App } from './app'
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-    }),
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 1.0,
-  tracePropagationTargets: ['https://as-crm-git-sentry-test-aservs-projects.vercel.app/'],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
+  tracePropagationTargets: [
+    'https://as-crm-git-sentry-test-aservs-projects.vercel.app/',
+    'as-crm-git-sentry-test-aservs-projects.vercel.app',
+    '.vercel.',
+    'app',
+    'localhost',
+  ],
+  beforeSend(event) {
+    console.log('Sentry event:', event)
+    return event
+  },
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
