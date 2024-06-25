5a0e5f91f19099291e4bd5840d7581c84f31276c
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-25 15:44:29 +0300

    sentry-test

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ import { reportWebVitals } from '@utils/report-web-vitals'
 import { App } from './app'
 
 Sentry.init({
-  dsn: 'https://9d93845486a53513477f1c4901b80625@o4507371916099584.ingest.de.sentry.io/4507446537158736',
+  dsn: process.env.REACT_APP_SENTRY_DSN,
   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ['as-crm-git-sentry-test-aservs-projects.vercel.app'],
