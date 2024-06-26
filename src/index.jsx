bed30200c1ec1c20cd84cb233d83e48f8670e6fa
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-06-26 15:40:39 +0300

    update sentry

--- src/index.jsx
+++ src/index.jsx
@@ -14,14 +14,14 @@ import { App } from './app'
 Sentry.init({
   dsn: 'https://9d93845486a53513477f1c4901b80625@o4507371916099584.ingest.de.sentry.io/4507446537158736', // research about process.env
   integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.browserProfilingIntegration(),
-    // Sentry.captureConsoleIntegration(), // all console logs
-    Sentry.contextLinesIntegration(),
+    // Sentry.browserTracingIntegration(),
+    // Sentry.browserProfilingIntegration(),
+    // Sentry.captureConsoleIntegration(), // tracking by all console.log
+    // Sentry.contextLinesIntegration(),
     // Sentry.debugIntegration(), // debugger-console.log in devtools
     Sentry.extraErrorDataIntegration(),
     Sentry.httpClientIntegration({
-      // failedRequestTargets: ['amazonapi.fvds.ru', 'amazon-socket.fvds.ru'],
+      // failedRequestTargets: ['amazonapi.fvds.ru', 'amazon-socket.fvds.ru'], // URLs for tracking, by default - all
       failedRequestStatusCodes: [
         [500, 599],
         [400, 499],
@@ -31,15 +31,16 @@ Sentry.init({
     Sentry.sessionTimingIntegration(),
     // add a router when updating routing
   ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ['as-crm-dev.vercel.app', 'as-crm-git-sentry-test-aservs-projects.vercel.app'], // research about process.env
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  release: appVersion,
-  environment: 'development',
+  tracesSampleRate: 1.0, // browserTracingIntegration
+  // tracePropagationTargets: ['as-crm-dev.vercel.app', 'as-crm-git-sentry-test-aservs-projects.vercel.app'], // URLs for tracking, research about process.env
+  allowUrls: ['as-crm-dev.vercel.app', 'as-crm-git-sentry-test-aservs-projects.vercel.app'],
+  replaysSessionSampleRate: 0.1, // Sentry.replayIntegration
+  replaysOnErrorSampleRate: 1.0, // Sentry.replayIntegration
+  release: appVersion, // research about process.env
+  environment: 'development', // research about process.env
   normalizeMaxBreadth: 200,
-  sendDefaultPii: true,
-  denyUrls: ['localhost'],
+  sendDefaultPii: true, // for Sentry.httpClientIntegration
+  denyUrls: ['localhost'], // URLs skip, research about process.env
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
