f453c1a295d732c3bc3be32c6dfd980b04d352da
Ulad <vladis67948@gmail.com>
2024-07-02 08:10:55 +0300

    10122-fix(buyer_orders): status group fixed

--- src/ui/views/buyer/buyer-orders-views/buyer-my-orders-view/helpers/get-status-group.ts
+++ src/ui/views/buyer/buyer-orders-views/buyer-my-orders-view/helpers/get-status-group.ts
@@ -2,8 +2,11 @@ import { routsPathes } from '@constants/navigation/routs-pathes'
 
 export const getStatusGroup = (pathname: string) => {
   switch (pathname) {
-    case routsPathes.BUYER_MY_ORDERS_INBOUND:
+    case routsPathes.BUYER_MY_ORDERS_NOT_PAID:
       return 'notPaid'
+
+    case routsPathes.BUYER_MY_ORDERS_INBOUND:
+      return 'inbound'
 
     case routsPathes.BUYER_MY_ORDERS_READY_FOR_PAYMENT:
       return 'readyForPayment'
