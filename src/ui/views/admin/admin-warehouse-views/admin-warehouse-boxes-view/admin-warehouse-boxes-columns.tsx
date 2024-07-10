cd320be4a762a479f1ff60c013dc0506c6be15e4
Ulad <vladis67948@gmail.com>
2024-07-10 12:44:39 +0300

    10236-fix(admin_boxes): column view fixed

--- src/ui/views/admin/admin-warehouse-views/admin-warehouse-boxes-view/admin-warehouse-boxes-columns.tsx
+++ src/ui/views/admin/admin-warehouse-views/admin-warehouse-boxes-view/admin-warehouse-boxes-columns.tsx
@@ -119,14 +119,16 @@ export const adminWarehouseBoxesColumns = () => {
       field: 'totalPrice',
       headerName: t(TranslationKey['Total price']),
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Total price'])} />,
-      renderCell: params => <MultilineTextCell text={toFixedWithDollarSign(params.row.amazonPrice, 2)} />,
+      renderCell: params => (
+        <MultilineTextCell text={toFixedWithDollarSign(params.row.items?.[0]?.order?.totalPrice, 2)} />
+      ),
       width: 110,
       disableCustomSort: true,
       filterable: false,
     },
 
     {
-      field: 'finalWeight',
+      field: 'weighGrossKgSupplier',
       headerName: t(TranslationKey['Final weight']),
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Final weight'])} />,
       renderCell: params => <MultilineTextCell text={toFixedWithKg(params.value, 2)} />,
@@ -151,7 +153,7 @@ export const adminWarehouseBoxesColumns = () => {
       field: 'trackNumberText',
       headerName: t(TranslationKey['Track number']),
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Track number'])} />,
-      renderCell: params => <MultilineTextCell text={params.row.trackNumberText} />,
+      renderCell: params => <MultilineTextCell text={params.value} />,
       columnKey: columnnsKeys.shared.NUMBER,
       disableCustomSort: true,
       width: 150,
