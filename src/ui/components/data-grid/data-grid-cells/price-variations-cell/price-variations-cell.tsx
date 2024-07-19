51a0a9ef1973f9573665b0b2a0e0f22278f760cf
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-07-19 14:22:28 +0300

    10318-fix(variationsCell): update styles

--- src/ui/components/data-grid/data-grid-cells/price-variations-cell/price-variations-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/price-variations-cell/price-variations-cell.tsx
@@ -21,21 +21,23 @@ export const PriceVariationsCell: FC<PriceVariationsCellProps> = memo(({ variati
   const { classes: styles } = useStyles()
 
   return (
-    <div className={styles.wrapper}>
-      {variations?.length > 0
-        ? variations.map((variation, index) => {
-            const variationValue = `${variation.quantity} ${t(TranslationKey['pcs.'])} / ${toFixedWithDollarSign(
-              variation.price / (yuanToDollarRate || 0),
-              2,
-            )} ${t(TranslationKey.Per).toLowerCase()} ${t(TranslationKey['pcs.'])}`
+    <div className={styles.root}>
+      <div className={styles.variations}>
+        {variations?.length > 0
+          ? variations.map((variation, index) => {
+              const variationValue = `${variation.quantity} ${t(TranslationKey['pcs.'])} / ${toFixedWithDollarSign(
+                variation.price / (yuanToDollarRate || 0),
+                2,
+              )} ${t(TranslationKey.Per).toLowerCase()} ${t(TranslationKey['pcs.'])}`
 
-            return (
-              <p key={index} className={styles.text}>
-                {variationValue}
-              </p>
-            )
-          })
-        : '-'}
+              return (
+                <p key={index} className={styles.text}>
+                  {variationValue}
+                </p>
+              )
+            })
+          : t(TranslationKey['No data'])}
+      </div>
     </div>
   )
 })
