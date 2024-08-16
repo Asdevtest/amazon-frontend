537edcb3e89a6cb55101b03cd1ae6a8aa58ac35f
Ulad <vladis67948@gmail.com>
2024-08-16 14:33:54 +0200

    10254-merge(table_presets): fyp out of stock columns

--- src/ui/views/client/parsing-reports/helpers/get-additional-filters-fields.ts
+++ src/ui/views/client/parsing-reports/helpers/get-additional-filters-fields.ts
@@ -3,13 +3,14 @@ import { ParsingReportsType } from '../parsing-reports.type'
 export const getAdditionalFiltersFields = (activeTable: ParsingReportsType) => {
   switch (activeTable) {
     case ParsingReportsType.VOICE:
-      return ['sku', 'amazonTitle']
-    case ParsingReportsType.ORDERS:
-      return ['sku']
-    case ParsingReportsType.INVENTORY:
-      return ['title', 'asin', 'sku']
+      return ['amazonTitle', 'sku']
 
+    case ParsingReportsType.INVENTORY:
+      return ['title', 'sku']
+
+    case ParsingReportsType.ORDERS:
     case ParsingReportsType.RETURNS:
+    case ParsingReportsType.FYP_OUT_OF_STOCK:
       return ['sku']
   }
 }
