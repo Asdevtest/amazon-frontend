c9841391456daaccdb1bc2507f3f529f116e7d50
Ulad <vladis67948@gmail.com>
2024-08-16 08:03:05 +0200

    10526-feat(order): table added

--- src/ui/views/client/parsing-reports/parsing-reports-columns/orders.columns.tsx
+++ src/ui/views/client/parsing-reports/parsing-reports-columns/orders.columns.tsx
@@ -1,13 +1,35 @@
 import { columnnsKeys } from '@constants/data-grid/data-grid-columns-keys'
+import { TranslationKey } from '@constants/translations/translation-key'
 
-import { MultilineTextHeaderCell, NormDateCell } from '@components/data-grid/data-grid-cells'
+import {
+  MultilineTextHeaderCell,
+  NormDateCell,
+  ProductAsinCell,
+  TextCell,
+  UserLinkCell,
+} from '@components/data-grid/data-grid-cells'
+
+import { toFixed } from '@utils/text'
+import { t } from '@utils/translations'
 
 import { IGridColumn } from '@typings/shared/grid-column'
+
+import { getProductColumnMenuItems, getProductColumnMenuValue } from '@config/data-grid-column-menu/product-column'
 
 import { ParsingReportsType } from '../parsing-reports.type'
 
 export const ordersColumns = () => {
   const columns: IGridColumn<ParsingReportsType>[] = [
+    {
+      field: 'dateUpdated',
+      headerName: 'Date updated',
+      renderHeader: () => <MultilineTextHeaderCell text="Date updated" />,
+      renderCell: params => <NormDateCell value={params.value} />,
+      width: 120,
+
+      columnKey: columnnsKeys.shared.DATE,
+    },
+
     {
       field: 'date',
       headerName: 'Date',
@@ -17,11 +39,86 @@ export const ordersColumns = () => {
 
       columnKey: columnnsKeys.shared.DATE,
     },
+
+    {
+      field: 'amzOrderId',
+      headerName: 'Amz order id',
+      renderHeader: () => <MultilineTextHeaderCell text="Amz order id" />,
+
+      renderCell: params => <TextCell text={params.value} />,
+      width: 115,
+      columnKey: columnnsKeys.shared.STRING_VALUE,
+    },
+
+    {
+      field: 'asin',
+      headerName: t(TranslationKey.ASIN),
+      renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.ASIN)} />,
+      renderCell: ({ row }) => <ProductAsinCell withoutImage withoutTitle asin={row?.asin} skuByClient={row?.sku} />,
+
+      fields: getProductColumnMenuItems({ withoutTitle: true }),
+      columnMenuConfig: getProductColumnMenuValue({ isSimpleSku: true }),
+      columnKey: columnnsKeys.shared.MULTIPLE,
+      width: 210,
+    },
+
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+      renderHeader: () => <MultilineTextHeaderCell text="Quantity" />,
+
+      renderCell: params => <TextCell text={toFixed(params.value)} />,
+      width: 115,
+      columnKey: columnnsKeys.shared.NUMBER,
+    },
+
+    {
+      field: 'sum',
+      headerName: 'Sum',
+      renderHeader: () => <MultilineTextHeaderCell text="Sum" />,
+
+      renderCell: params => <TextCell text={toFixed(params.value)} />,
+      width: 115,
+      columnKey: columnnsKeys.shared.NUMBER,
+    },
+
+    {
+      field: 'status',
+      headerName: 'Status',
+      renderHeader: () => <MultilineTextHeaderCell text="Status" />,
+
+      renderCell: params => <TextCell text={params.value} />,
+      width: 115,
+      columnKey: columnnsKeys.shared.STRING_VALUE,
+    },
+
+    {
+      field: 'shop',
+      headerName: t(TranslationKey.Shop),
+      renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.Shop)} />,
+
+      renderCell: params => <TextCell text={params.row?.shop?.name} />,
+      width: 90,
+      columnKey: columnnsKeys.shared.OBJECT_VALUE,
+      disableCustomSort: true,
+    },
+
+    {
+      field: 'client',
+      headerName: t(TranslationKey['Created by']),
+      renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Created by'])} />,
+
+      renderCell: params => <UserLinkCell blackText name={params.row.client?.name} userId={params.row.client?._id} />,
+      width: 110,
+
+      columnKey: columnnsKeys.shared.OBJECT_VALUE,
+      disableCustomSort: true,
+    },
   ]
 
   for (const column of columns) {
     if (!column.table) {
-      column.table = ParsingReportsType.BUSINESS_REPORTS
+      column.table = ParsingReportsType.ORDERS
     }
 
     column.sortable = false
