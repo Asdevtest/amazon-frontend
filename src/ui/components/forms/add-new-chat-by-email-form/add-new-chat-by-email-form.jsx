061e4032ae94fd26fa68955544724ee1eb491bdf
Ulad <vladis67948@gmail.com>
2024-08-20 07:57:51 +0200

    10254-feat-presets-add_custom_server_save_settings

--- src/ui/components/forms/add-new-chat-by-email-form/add-new-chat-by-email-form.jsx
+++ src/ui/components/forms/add-new-chat-by-email-form/add-new-chat-by-email-form.jsx
@@ -22,7 +22,6 @@ export const AddNewChatByEmailForm = memo(({ closeModal, onSubmit, usersData }) 
 
   const [userDataForRender, setUserDataForRender] = useState([])
   const [submitIsClicked, setSubmitIsClicked] = useState(false)
-
   const [formFields, setFormFields] = useState({ chosenUsers: [], title: '', images: [] })
 
   const disableSubmit =
