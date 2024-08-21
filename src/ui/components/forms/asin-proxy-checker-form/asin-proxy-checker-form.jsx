ee3ce02bfc31e754a839214ceeab3342dcb961d2
Ulad <vladis67948@gmail.com>
2024-08-21 12:19:15 +0200

    10473-fix(add_asin_modal): empty value fixed

--- src/ui/components/forms/asin-proxy-checker-form/asin-proxy-checker-form.jsx
+++ src/ui/components/forms/asin-proxy-checker-form/asin-proxy-checker-form.jsx
@@ -53,11 +53,13 @@ export const AsinProxyCheckerForm = ({ user, strategy, onSubmit, onClose }) => {
     } else {
       asinsData.length &&
         asinsData.forEach((item, index) => {
-          data.push({
-            asin: item.trim(),
-            reason: reasonsData[index] || '',
-            strategy: mapProductStrategyStatusEnumToKey[strategy],
-          })
+          if (item) {
+            data.push({
+              asin: item.trim(),
+              reason: reasonsData[index] || '',
+              strategy: mapProductStrategyStatusEnumToKey[strategy],
+            })
+          }
         })
     }
 
