dc7467fd31a9d98f9eeffaaa7dc90c9b996bebce
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-08-22 13:02:29 +0300

    10585-fix(shops): add message

--- src/constants/translations/translation-key.ts
+++ src/constants/translations/translation-key.ts
@@ -2004,7 +2004,6 @@ export enum TranslationKey {
   'Access' = 'Access',
   'Issued' = 'Issued',
   'Parsing status' = 'Parsing status',
-  'Profile with given guid not found!' = 'Profile with given guid not found!',
   'Access Denied: Insufficient Rights' = 'Access Denied: Insufficient Rights',
   'Ask' = 'Ask',
   'Please enter a valid URL!' = 'Please enter a valid URL!',
