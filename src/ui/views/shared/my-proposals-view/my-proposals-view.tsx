e335774f2b70fc918bd16de549d68715cc33c7fc
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-08-29 17:45:07 +0600

    10642-fix(proposalsTable): fix modal

--- src/ui/views/shared/my-proposals-view/my-proposals-view.tsx
+++ src/ui/views/shared/my-proposals-view/my-proposals-view.tsx
@@ -102,7 +102,7 @@ export const MyProposalsView = observer(({ allProposals }: { allProposals: boole
           onFilterModelChange={viewModel.onChangeFilterModel}
           onColumnVisibilityModelChange={viewModel.onColumnVisibilityModelChange}
           onPaginationModelChange={viewModel.onPaginationModelChange}
-          onRowClick={(params: GridRowParams) => viewModel.onOpenRequestDetailModal(params.row._id)}
+          onRowClick={(params: GridRowParams) => viewModel.onOpenRequestDetailModal(params.row.request?._id)}
         />
       </div>
 
