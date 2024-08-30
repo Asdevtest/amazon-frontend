70cb7e1367d0517f939b2a6859cafc3143ab04ba
Ulad <vladis67948@gmail.com>
2024-08-30 13:51:17 +0200

    10254-task(warehouse): added presets

--- src/ui/views/warehouse/warehouse-batches-views/warehouse-my-batches-view/warehouse-my-batches-view.tsx
+++ src/ui/views/warehouse/warehouse-batches-views/warehouse-my-batches-view/warehouse-my-batches-view.tsx
@@ -123,6 +123,16 @@ export const WarehouseMyBatchesView: FC<WarehouseMyBatchesViewProps> = observer(
               columnsModel: viewModel.columnsModel,
               onSortModelChange: viewModel.onChangeSortingModel,
             },
+            tablePresets: {
+              showPresetsSelect: viewModel.showPresetsSelect,
+              presetsTableData: viewModel.presetsTableData,
+              handleChangeSelectState: viewModel.onChangeShowPresetsSelect,
+              handleSetPresetActive: viewModel.handleSetPresetActive,
+              handleCreateTableSettingsPreset: viewModel.handleCreateTableSettingsPreset,
+              handleDeleteTableSettingsPreset: viewModel.handleDeleteTableSettingsPreset,
+              handleUpdateTableSettingsPreset: viewModel.handleUpdateTableSettingsPreset,
+              onClickAddQuickAccess: viewModel.onClickAddQuickAccess,
+            },
           },
         }}
         rowCount={viewModel.rowCount}
