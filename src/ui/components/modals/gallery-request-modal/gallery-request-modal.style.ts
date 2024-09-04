b79eefd86ab2e7bb3e48f78446387499cbacf637
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-09-04 13:12:33 +0600

    10653-feat(requestModal): add select all checkbox

--- src/ui/components/modals/gallery-request-modal/gallery-request-modal.style.ts
+++ src/ui/components/modals/gallery-request-modal/gallery-request-modal.style.ts
@@ -4,4 +4,9 @@ export const useStyles = makeStyles()(() => ({
   wrapper: {
     width: 870,
   },
+  headerContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
 }))
