e7f226680139f6b8711c24f8ced520018a1fd0f2
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-09-16 13:00:48 +0600

    10753-feat(redFlagsCell): add popover

--- src/ui/components/data-grid/data-grid-cells/red-flags-cell/red-flags-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/red-flags-cell/red-flags-cell.tsx
@@ -1,3 +1,4 @@
+import { Popover } from 'antd'
 import { FC, memo } from 'react'
 
 import { RedFlags } from '@components/shared/redFlags/red-flags'
@@ -13,9 +14,26 @@ interface RedFlagsCellProps {
 export const RedFlagsCell: FC<RedFlagsCellProps> = memo(({ flags }) => {
   const { classes: styles } = useStyles()
 
+  const visibleFlags = flags.slice(0, 5)
+  const hiddenFlags = flags.slice(5)
+  const hiddenFlagsCount = hiddenFlags.length
+
   return (
-    <div className={styles.redFlags}>
-      <RedFlags activeFlags={flags} />
-    </div>
+    <Popover
+      content={
+        hiddenFlagsCount > 0 && (
+          <div className={styles.hiddenFlagsPopover}>
+            <RedFlags activeFlags={hiddenFlags} />
+          </div>
+        )
+      }
+      trigger="hover"
+      placement="top"
+    >
+      <div className={styles.redFlags}>
+        <RedFlags activeFlags={visibleFlags} />
+        {hiddenFlagsCount > 0 && <span className={styles.moreFlags}>+{hiddenFlagsCount}</span>}
+      </div>
+    </Popover>
   )
 })
