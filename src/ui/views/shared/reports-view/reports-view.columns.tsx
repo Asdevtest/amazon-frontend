004e86c9fb17d64f51e96da88a9ff9819276689c
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-09-17 17:11:01 +0600

    10770-fix(reportsView): fix column

--- src/ui/views/shared/reports-view/reports-view.columns.tsx
+++ src/ui/views/shared/reports-view/reports-view.columns.tsx
@@ -209,7 +209,7 @@ export const reportsViewColumns = (props: ReportsViewColumnsProps) => {
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey.Comment)} />,
       renderCell: ({ row }: GridRowModel) => <Text isCell text={row.description} />,
       flex: 1,
-      columnKey: columnnsKeys.shared.STRING,
+      columnKey: columnnsKeys.shared.STRING_VALUE,
     },
 
     {
