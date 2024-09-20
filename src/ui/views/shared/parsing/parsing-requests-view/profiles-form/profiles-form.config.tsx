868a6c1afd1799be22b97159e31dc9db61063489
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-09-20 11:04:28 +0300

    10799-feat(parsingViews): update

--- src/ui/views/shared/parsing/parsing-requests-view/profiles-form/profiles-form.config.tsx
+++ src/ui/views/shared/parsing/parsing-requests-view/profiles-form/profiles-form.config.tsx
@@ -1,6 +1,9 @@
 import { action, computed, observable } from 'mobx'
+import { BiUnlink } from 'react-icons/bi'
 
 import { Text } from '@components/shared/text'
+
+import { ProfileStatus } from '@typings/enums/request/profile-request-status'
 
 import { IPermissionsData } from '@hooks/use-products-permissions'
 
@@ -14,9 +17,12 @@ export const profilesFormConfig = {
 export const searchFields = ['name', 'email']
 
 export const getProfilesOptions = (profiles: IPermissionsData[]) =>
-  profiles.map(profile => ({
-    label: (
-      <Text textRows={1} copyable={false} text={`${profile.email} (${profile.name})`} textStyle={{ maxWidth: 350 }} />
-    ),
-    value: profile._id,
-  }))
+  profiles.map(profile => {
+    const text = `${profile.email} (${profile.name})`
+    const icon = profile.status === ProfileStatus.UNLINKED ? <BiUnlink size={12} /> : null
+
+    return {
+      label: <Text textRows={1} icon={icon} copyable={false} text={text} textStyle={{ maxWidth: 350 }} />,
+      value: profile._id,
+    }
+  })
