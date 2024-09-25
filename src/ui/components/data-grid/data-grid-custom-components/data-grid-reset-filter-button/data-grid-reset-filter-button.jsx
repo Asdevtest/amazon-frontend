7e13df0654426530ba8fbcbef48e610972767955
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-09-25 18:58:14 +0600

    10811-fix(resetFilterButton): change icon

--- src/ui/components/data-grid/data-grid-custom-components/data-grid-reset-filter-button/data-grid-reset-filter-button.jsx
+++ src/ui/components/data-grid/data-grid-custom-components/data-grid-reset-filter-button/data-grid-reset-filter-button.jsx
@@ -1,6 +1,6 @@
 import { t } from 'i18n-js'
 import { memo } from 'react'
-import { MdOutlineFilterAlt } from 'react-icons/md'
+import { MdOutlineFilterAltOff } from 'react-icons/md'
 
 import { Typography } from '@mui/material'
 
@@ -27,7 +27,7 @@ export const DataGridResetFilterButton = memo(props => {
         {...restProps}
       >
         <div className={cx(className, styles.mainFilterBtnInsert)}>
-          <MdOutlineFilterAlt size={20} />
+          <MdOutlineFilterAltOff size={20} />
 
           <Typography className={styles.mainFilterBtnInsertText}>{t(TranslationKey['Reset filters'])}</Typography>
         </div>
