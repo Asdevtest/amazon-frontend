e3bc94f1414bc69630371d53f5bb9a38f787229e
Ulad <vladis67948@gmail.com>
2024-09-26 08:14:56 +0200

    refactor(websocket_spacename): added config

--- src/services/websocket/websocket-spacename/observer.config.ts
+++ src/services/websocket/websocket-spacename/observer.config.ts
@@ -1,5 +1,21 @@
-import { observable } from 'mobx'
+import { action, observable } from 'mobx'
 
 export const observerConfig = {
   socket: observable,
+  manager: observable,
+  namespace: observable,
+  handlersToRegister: observable,
+  defaultHandlersToRegister: observable,
+
+  init: action.bound,
+
+  disconnect: action.bound,
+
+  onConnect: action.bound,
+  onDisconnect: action.bound,
+  onError: action.bound,
+
+  registerHandlers: action.bound,
+
+  ping: action.bound,
 }
