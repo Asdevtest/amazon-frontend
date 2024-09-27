ef50f9890974aca465d5fbc7ae89a43ea415bc7c
Ulad <vladis67948@gmail.com>
2024-09-27 15:13:40 +0200

    10807-refactor(chat): added ui

---
+++ src/ui/views/shared/chat-view/components/chat-item/chat-item.style.ts
@@ -0,0 +1,58 @@
+import { makeStyles } from 'tss-react/mui'
+
+export const useStyles = makeStyles()(theme => ({
+  chatItem: {
+    borderRadius: '0px',
+    overflow: 'hidden',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding: '5px 10px',
+    height: '60px',
+    minHeight: '60px',
+    width: '100%',
+    gap: '10px',
+  },
+
+  avatar: {
+    height: '48px !important',
+    minWidth: '48px !important',
+  },
+
+  favoritesIcon: {
+    color: theme.palette.primary.main,
+  },
+
+  titleWrapper: {
+    flex: '0 1 auto',
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '10px',
+    alignItems: 'center',
+  },
+
+  text: {
+    width: '100%',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    textAlign: 'start',
+  },
+
+  title: {
+    fontWeight: '600',
+    fontSize: '16px',
+  },
+
+  lastMessageDate: {
+    fontSize: '12px',
+    color: theme.palette.text.second,
+  },
+
+  chatItemInfo: {
+    overflow: 'hidden',
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+}))
---
+++ src/ui/views/shared/chat-view/components/chat-item/chat-item.tsx
@@ -0,0 +1,62 @@
+import { Avatar } from 'antd'
+import { FC, memo, useCallback, useMemo } from 'react'
+
+import { ChatsType } from '@constants/keys/chats'
+
+import { Chat } from '@models/chat-model-new/types/chat.type'
+
+import { CustomButton } from '@components/shared/custom-button'
+import { FavoritesIcon } from '@components/shared/svg-icons/favorites-icon/favorites-icon'
+
+import { getChatTitle } from '@utils/chat/get-chat-title'
+import { formatDateWithoutTimeLocal } from '@utils/date-time'
+import { getAmazonImageUrl } from '@utils/get-amazon-image-url'
+import { getUserAvatarSrc } from '@utils/get-user-avatar'
+
+import { useStyles } from './chat-item.style'
+
+interface ChatItemProps {
+  chat: Chat
+  onClickChat: (chat: Chat) => void
+}
+
+export const ChatItem: FC<ChatItemProps> = memo(({ chat, onClickChat }) => {
+  const { classes: styles, cx } = useStyles()
+
+  const isFavoritesChat = useMemo(() => chat.type === ChatsType.SAVED, [])
+  const isGroupChat = useMemo(() => chat.type === ChatsType.GROUP, [])
+
+  const avatarSrc = useMemo(() => {
+    if (isGroupChat) {
+      return getAmazonImageUrl(chat.info?.image)
+    } else {
+      return getUserAvatarSrc(chat.users[0]._id)
+    }
+  }, [])
+
+  const chatTitle = useMemo(() => getChatTitle(chat), [])
+  const lastMessageDate = useMemo(() => formatDateWithoutTimeLocal(chat.lastMessage?.updatedAt), [chat.lastMessage])
+
+  const onClick = useCallback(() => {
+    onClickChat(chat)
+  }, [])
+
+  return (
+    <CustomButton type="text" className={styles.chatItem} onClick={onClick}>
+      {isFavoritesChat ? (
+        <FavoritesIcon className={cx(styles.favoritesIcon, styles.avatar)} />
+      ) : (
+        <Avatar shape="circle" src={avatarSrc} className={styles.avatar} />
+      )}
+
+      <div className={styles.chatItemInfo}>
+        <div className={styles.titleWrapper}>
+          <p className={cx(styles.text, styles.title)}>{chatTitle}</p>
+          <p className={styles.lastMessageDate}>{lastMessageDate}</p>
+        </div>
+
+        <p className={styles.text}>{chat.lastMessage?.text}</p>
+      </div>
+    </CustomButton>
+  )
+})
