0d26d685f463e8480d918d02f23c21cfcb6f831d
Ulad <vladis67948@gmail.com>
2024-09-30 13:19:08 +0200

    10807-refactor(empty_chat): added component

---
+++ src/ui/views/shared/chat-view/components/empty-chat-messages/empty-chat-messages.tsx
@@ -0,0 +1,19 @@
+import { Empty } from 'antd'
+import { observer } from 'mobx-react'
+import { FC } from 'react'
+
+import { TranslationKey } from '@constants/translations/translation-key'
+
+import { chatModel } from '@models/chat-model-new/chat-model'
+
+import { t } from '@utils/translations'
+
+export const EmptyChatMessages: FC = observer(() => {
+  const isSelectedChat = !!chatModel.selectedChatId
+
+  const message = isSelectedChat
+    ? 'Select a chat room to start messaging'
+    : 'Looks like no one has chatted here yet. Send a message to get started!'
+
+  return <Empty description={t(TranslationKey[message])} />
+})
