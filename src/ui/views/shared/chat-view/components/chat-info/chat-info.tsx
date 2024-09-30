e8b42b7fe21170daf28c9918e92eb0f5d682786b
Ulad <vladis67948@gmail.com>
2024-09-30 07:52:28 +0200

    refactor(chat_info): init

---
+++ src/ui/views/shared/chat-view/components/chat-info/chat-info.tsx
@@ -0,0 +1,9 @@
+import { observer } from 'mobx-react'
+
+import { useStyles } from './chat-info.style'
+
+export const ChatInfo = observer(() => {
+  const { classes: styles, cx } = useStyles()
+
+  return <div></div>
+})
