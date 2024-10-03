bfb8829cd42a80914fa0482f2d9d7df013d47b4f
Ulad <vladis67948@gmail.com>
2024-10-03 15:06:16 +0200

    10807-refactor(chat): added chat header

---
+++ src/ui/views/shared/chat-view/components/chat-header/chat-header.tsx
@@ -0,0 +1,29 @@
+import { observer } from 'mobx-react'
+import { FC } from 'react'
+import { TbLayoutSidebarRightCollapse } from 'react-icons/tb'
+
+import { chatModel } from '@models/chat-model-new/chat-model'
+import { Chat } from '@models/chat-model-new/types/chat.type'
+
+import { CustomButton } from '@components/shared/custom-button'
+import { CustomInputSearch } from '@components/shared/custom-input-search'
+
+import { useStyles } from './chat-header.styles'
+
+import { ChatInfoUser } from '../chat-info-user'
+
+export const ChatHeader: FC = observer(() => {
+  const { classes: styles } = useStyles()
+
+  const currentChat = chatModel.currentChat as Chat
+
+  return (
+    <div className={styles.chatInfoHeaderWrapper}>
+      <ChatInfoUser currentChat={currentChat} />
+
+      <CustomInputSearch placeholder="Search" />
+
+      <CustomButton type="text" icon={<TbLayoutSidebarRightCollapse size={20} />} />
+    </div>
+  )
+})
