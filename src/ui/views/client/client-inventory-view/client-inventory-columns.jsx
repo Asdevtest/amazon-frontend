42a0e279101a0f800b96bea560900b008fd190de
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-10-04 17:23:37 +0600

    10831-fix(clientInventory): fix column

--- src/ui/views/client/client-inventory-view/client-inventory-columns.jsx
+++ src/ui/views/client/client-inventory-view/client-inventory-columns.jsx
@@ -176,7 +176,7 @@ export const clientInventoryColumns = ({
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['Pending order'])} />,
       renderCell: params => <OrderIdAndAmountCountCell amount={params.row?.amountInPendingOrders} />,
       width: 85,
-      columnKey: columnnsKeys.shared.NUMBER,
+      columnKey: columnnsKeys.shared.STRING,
     },
 
     {
