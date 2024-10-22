5b765bca222a0261a56de89638f856d27dc37470
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-10-22 16:46:40 +0300

    11036-fix(stringListCell):fix styles

--- src/ui/components/data-grid/data-grid-cells/string-list-cell/string-list-cell.tsx
+++ src/ui/components/data-grid/data-grid-cells/string-list-cell/string-list-cell.tsx
@@ -7,14 +7,13 @@ import { StringItem } from './string-item'
 interface StringListCellProps {
   data: string[]
   asin?: boolean
-  fixedHeight?: boolean
 }
 
-export const StringListCell: FC<StringListCellProps> = memo(({ data = [], asin, fixedHeight = true }) => {
-  const { cx, classes: styles } = useStyles()
+export const StringListCell: FC<StringListCellProps> = memo(({ data = [], asin }) => {
+  const { classes: styles } = useStyles()
 
   return (
-    <div className={cx(styles.root, { [styles.fixedHeight]: fixedHeight })}>
+    <div className={styles.root}>
       {data?.map((item, index) => (
         <StringItem key={index} item={item} asin={asin} />
       ))}
