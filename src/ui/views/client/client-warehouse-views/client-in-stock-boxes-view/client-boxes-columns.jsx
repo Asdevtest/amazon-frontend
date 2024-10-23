dcfa20d46556f771407d0262d8dbb6f9de607203
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-10-23 20:05:15 +0600

    11035-fix(clientBoxes): fix column

--- src/ui/views/client/client-warehouse-views/client-in-stock-boxes-view/client-boxes-columns.jsx
+++ src/ui/views/client/client-warehouse-views/client-in-stock-boxes-view/client-boxes-columns.jsx
@@ -99,10 +99,8 @@ export const clientBoxesViewColumns = (
       headerName: t(TranslationKey['№ Order']),
       renderHeader: () => <MultilineTextHeaderCell text={t(TranslationKey['№ Order'])} />,
 
-      renderCell: params => {
-        return <StringListCell data={params.row.items?.map(item => item.order?.xid)} />
-      },
-      valueGetter: ({ row }) => row.items?.map(item => item.order?.xid),
+      renderCell: params => params.row.items?.map(item => item.order?.xid).join(', '),
+      valueGetter: ({ row }) => row.items?.map(item => item.order?.xid).join(', '),
       width: 160,
 
       columnKey: columnnsKeys.shared.NUMBER,
