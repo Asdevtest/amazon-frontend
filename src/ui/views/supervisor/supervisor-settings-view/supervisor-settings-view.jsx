93a5ac49013d77d11fde4554a861be87ee1f2b99
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-11-01 19:15:46 +0600

    11152-fix(supervisorSettings): fix styles

--- src/ui/views/supervisor/supervisor-settings-view/supervisor-settings-view.jsx
+++ src/ui/views/supervisor/supervisor-settings-view/supervisor-settings-view.jsx
@@ -37,7 +37,6 @@ export const SupervisorSettingsView = observer(() => {
         <CustomInputSearch
           enterButton
           allowClear
-          size="large"
           placeholder="Search by ASIN, Reason"
           onSearch={viewModel.onChangeUnserverSearchValue}
         />
@@ -45,7 +44,6 @@ export const SupervisorSettingsView = observer(() => {
         <div className={styles.flexContainer}>
           <CustomButton
             danger
-            size="large"
             type="primary"
             disabled={!viewModel.selectedRows.length}
             confirmText="Are you sure you want to delete the selected ASINs?"
@@ -54,11 +52,7 @@ export const SupervisorSettingsView = observer(() => {
             {t(TranslationKey['Delete selected ASINs'])}
           </CustomButton>
 
-          <CustomButton
-            size="large"
-            type="primary"
-            onClick={() => viewModel.onTriggerOpenModal('showAsinCheckerModal')}
-          >
+          <CustomButton type="primary" onClick={() => viewModel.onTriggerOpenModal('showAsinCheckerModal')}>
             ASIN checker
           </CustomButton>
         </div>
