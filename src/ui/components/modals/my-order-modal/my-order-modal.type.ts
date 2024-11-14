0b34c08a22b6cf22ece8513b689d189ddff86596
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-11-14 20:06:16 +0600

    fix(suppliers): fix fields

--- src/ui/components/modals/my-order-modal/my-order-modal.type.ts
+++ src/ui/components/modals/my-order-modal/my-order-modal.type.ts
@@ -1,11 +1,13 @@
 import { Dispatch, SetStateAction } from 'react'
 
 import { IOrder } from '@typings/models/orders/order'
+import { ISupplier } from '@typings/models/suppliers/supplier'
 
 export interface IOrderWithAdditionalFields extends IOrder {
   destinationId: string | null
   storekeeperId: string
   tmpBarCode: string[]
+  currentSupplierCard?: ISupplier
 }
 
 export type SetFormFieldsType = Dispatch<SetStateAction<IOrderWithAdditionalFields>>
