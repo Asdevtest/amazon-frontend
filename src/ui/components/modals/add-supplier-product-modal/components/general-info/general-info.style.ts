c232326747a0c5500aaf622f527d66ffe931fcdc
Ulad <vladis67948@gmail.com>
2024-11-14 08:02:30 +0100

    11099-feat(suppliers): general info

--- src/ui/components/modals/add-supplier-product-modal/components/general-info/general-info.style.ts
+++ src/ui/components/modals/add-supplier-product-modal/components/general-info/general-info.style.ts
@@ -1,3 +1,29 @@
 import { makeStyles } from 'tss-react/mui'
 
-export const useStyles = makeStyles()(() => ({}))
+export const useStyles = makeStyles()(() => ({
+  root: {
+    display: 'flex',
+    gap: '25px',
+  },
+
+  productInfoWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
+    flex: 1,
+  },
+
+  selectsWrapper: {
+    display: 'flex',
+    gap: '10px',
+    width: '100%',
+
+    '.select': {
+      flex: 1,
+    },
+  },
+
+  uploadFiles: {
+    flex: 1,
+  },
+}))
--- src/ui/components/modals/add-supplier-product-modal/components/general-info/general-info.tsx
+++ src/ui/components/modals/add-supplier-product-modal/components/general-info/general-info.tsx
@@ -1,8 +1,13 @@
 import { Form } from 'antd'
-import { memo } from 'react'
+import { FC, memo } from 'react'
 
 import { CustomInput } from '@components/shared/custom-input'
 import { CustomSelect } from '@components/shared/custom-select'
+import { CustomTreeSelect } from '@components/shared/custom-tree-select'
+import { UploadFilesInput } from '@components/shared/upload-files-input'
+
+import { ICategory } from '@typings/models/others/category'
+import { UploadFileType } from '@typings/shared/upload-file'
 
 import { getRequiredRules } from '@config/form-rules/get-required-rules'
 
@@ -11,48 +16,76 @@ import { useStyles } from './general-info.style'
 
 import { ICreateSupplierProduct } from '../../add-supplier-product-modal.type'
 
-export const GeneralInfo = memo(() => {
-  const { classes: styles } = useStyles()
+interface GeneralInfoProps {
+  categoriesLoading: boolean
+  categories: ICategory[]
+  images: UploadFileType[]
+  handleUploadFiles: (images: UploadFileType[]) => void
+}
+
+export const GeneralInfo: FC<GeneralInfoProps> = memo(props => {
+  const { categoriesLoading, categories, images, handleUploadFiles } = props
+
+  const { classes: styles, cx } = useStyles()
   const { classes: sharedStyles } = useSharedStyles()
 
   return (
-    <div>
-      <div>
-        <Form.Item<ICreateSupplierProduct> name="supplierId" className={sharedStyles.field} rules={getRequiredRules()}>
-          <CustomSelect
-            required
-            allowClear
-            mode="tags"
-            // loading={viewModel.paymentMethodsRequestStatus === loadingStatus.IS_LOADING}
-            wrapperClassName={sharedStyles.input}
-            label="Payment methods"
-            // options={viewModel.paymentMethods}
-            options={[{ label: 'test', value: 'test' }]}
-            // fieldNames={{ label: 'title', value: '_id' }}
-          />
+    <div className={styles.root}>
+      <div className={styles.productInfoWrapper}>
+        <div className={styles.selectsWrapper}>
+          <Form.Item<ICreateSupplierProduct>
+            name="supplierId"
+            className={cx(sharedStyles.field, 'select')}
+            rules={getRequiredRules()}
+          >
+            <CustomSelect
+              required
+              allowClear
+              mode="tags"
+              // loading={viewModel.paymentMethodsRequestStatus === loadingStatus.IS_LOADING}
+              wrapperClassName={sharedStyles.input}
+              label="Supplier"
+              placeholder="Supplier"
+              // options={viewModel.paymentMethods}
+              options={[{ label: 'test', value: 'test' }]}
+              // fieldNames={{ label: 'title', value: '_id' }}
+            />
+          </Form.Item>
+
+          <Form.Item<ICreateSupplierProduct>
+            name="categoryId"
+            className={cx(sharedStyles.field, 'select')}
+            rules={getRequiredRules()}
+          >
+            <CustomTreeSelect
+              required
+              allowClear
+              loading={categoriesLoading}
+              label="Categories"
+              placeholder="Categories"
+              wrapperClassName={sharedStyles.input}
+              treeData={categories}
+              fieldNames={{ label: 'title', value: '_id', children: 'subCategories' }}
+            />
+          </Form.Item>
+        </div>
+
+        <Form.Item<ICreateSupplierProduct> name="cardName" className={sharedStyles.field} rules={getRequiredRules()}>
+          <CustomInput required size="large" label="Title" placeholder="Title" wrapperClassName={sharedStyles.input} />
         </Form.Item>
 
-        <Form.Item<ICreateSupplierProduct> name="categoryId" className={sharedStyles.field} rules={getRequiredRules()}>
-          <CustomSelect
-            required
-            allowClear
-            mode="tags"
-            // loading={viewModel.paymentMethodsRequestStatus === loadingStatus.IS_LOADING}
-            wrapperClassName={sharedStyles.input}
-            label="Payment methods"
-            // options={viewModel.paymentMethods}
-            options={[{ label: 'test', value: 'test' }]}
-            // fieldNames={{ label: 'title', value: '_id' }}
-          />
+        <Form.Item<ICreateSupplierProduct> name="link" className={sharedStyles.field} rules={getRequiredRules()}>
+          <CustomInput required size="large" label="Link" placeholder="Link" wrapperClassName={sharedStyles.input} />
         </Form.Item>
       </div>
 
-      <Form.Item<ICreateSupplierProduct> name="cardName" className={sharedStyles.field} rules={getRequiredRules()}>
-        <CustomInput required size="large" placeholder="Fullname" wrapperClassName={sharedStyles.input} />
-      </Form.Item>
-
-      <Form.Item<ICreateSupplierProduct> name="cardName" className={sharedStyles.field} rules={getRequiredRules()}>
-        <CustomInput required size="large" placeholder="Fullname" wrapperClassName={sharedStyles.input} />
+      <Form.Item<ICreateSupplierProduct>
+        name="images"
+        className={cx(sharedStyles.field, styles?.uploadFiles)}
+        rules={getRequiredRules()}
+        validateTrigger={['onChange', 'onBlur']}
+      >
+        <UploadFilesInput dragAndDropButtonHeight={50} images={images} setImages={handleUploadFiles} />
       </Form.Item>
     </div>
   )
