e9fb3ce116b75b1ff43cd7ef6323454479fe479f
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-11-15 13:19:34 +0300

    add empty method

--- src/ui/views/shared/supplier-view/supplier-view.model.ts
+++ src/ui/views/shared/supplier-view/supplier-view.model.ts
@@ -1,4 +1,4 @@
-import { makeObservable } from 'mobx'
+import { makeObservable, runInAction } from 'mobx'
 import { toast } from 'react-toastify'
 
 import { TranslationKey } from '@constants/translations/translation-key'
@@ -7,13 +7,28 @@ import { ClientModel } from '@models/client-model'
 
 import { t } from '@utils/translations'
 
+import { ISupplierExchange } from '@typings/models/suppliers/supplier-exchange'
+
 import { supplierConfig } from './supplier-view.config'
 
 export class SupplierViewModel {
   showSelectShopsModal = false
+  suppplier?: ISupplierExchange
 
   constructor() {
     makeObservable(this, supplierConfig)
+  }
+
+  async onGetSupplier(id: string) {
+    try {
+      // const response = await ClientModel.getSupplier(id)
+
+      runInAction(() => {
+        // this.suppplier = response
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   onToggleSelectShopsModal() {
