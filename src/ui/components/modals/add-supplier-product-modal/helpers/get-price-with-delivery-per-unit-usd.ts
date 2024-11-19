e20250e86a51279ab9d16460cd19f819ee931418
Ulad <vladis67948@gmail.com>
2024-11-19 16:43:21 +0100

    11099-feat(supplier): added helpers

--- src/ui/components/modals/add-supplier-product-modal/helpers/get-price-with-delivery-per-unit-usd.ts
+++ src/ui/components/modals/add-supplier-product-modal/helpers/get-price-with-delivery-per-unit-usd.ts
@@ -1,6 +1,6 @@
-import { ICreateSupplierProduct } from '../add-supplier-product-modal.type'
+import { ICreateSupplierProductModal } from '../add-supplier-product-modal.type'
 
 import { getBatchPriceUsd } from './get-batch-price-usd'
 
-export const getPriceWithDeliveryPerUnitUsd = (values: ICreateSupplierProduct) =>
+export const getPriceWithDeliveryPerUnitUsd = (values: ICreateSupplierProductModal) =>
   getBatchPriceUsd(values) / Number(values.amount)
