038468126dc2f6bdfb65b0420a043125d8f3924c
oleg melekh <oleg.melekh.frontend@gmail.com>
2024-12-02 12:41:26 +0300

    11390/11397-fix(cards): remove logs

--- src/config/service-worker.ts
+++ src/config/service-worker.ts
@@ -1,10 +1,3 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js', { scope: '/' })
-    .then(() => {
-      console.log('Service Worker зарегистрирован')
-    })
-    .catch(() => {
-      console.log('Service Worker регистрация не удалась')
-    })
+  navigator.serviceWorker.register('/sw.js', { scope: '/' })
 }
