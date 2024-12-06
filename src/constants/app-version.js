f48c75e47b5791a3e7e061e5d8918a9fed220973
Ulad <vladis67948@gmail.com>
2024-12-06 13:49:03 +0100

    merge(pre-release): up to v4.23.5480

--- src/constants/app-version.js
+++ src/constants/app-version.js
@@ -1,1 +1,1 @@
-export const appVersion = 'v4.23.5400' // КАЖДЫЙ РАЗ ОБНОВЛЯЙ И В manifest.json и ServiceWorker
+export const appVersion = 'v4.23.5480' // КАЖДЫЙ РАЗ ОБНОВЛЯЙ И В manifest.json и ServiceWorker
