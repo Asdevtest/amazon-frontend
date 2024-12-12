1a8b4456934c65dfe517616a64e3050c4783bd2b
Dastan <96249607+tamashi22@users.noreply.github.com>
2024-12-12 17:09:51 +0600

    11525-feat(customField): add component

---
+++ src/ui/components/shared/custom-field/custom-field.tsx
@@ -0,0 +1,29 @@
+import { Tooltip } from 'antd'
+import { FC, memo } from 'react'
+
+import { TranslationKey } from '@constants/translations/translation-key'
+
+import { t } from '@utils/translations'
+
+import { IDefaultPropsExtensionAntdComponent } from '@typings/shared/default-props-extension-component-antd'
+
+import { useStyles } from './custom-field.style'
+
+interface CustomFieldProps extends IDefaultPropsExtensionAntdComponent {}
+
+export const CustomField: FC<CustomFieldProps> = memo(props => {
+  const { children, isRow, label, required, labelClassName, wrapperClassName, tooltip } = props
+
+  const { classes: styles, cx } = useStyles()
+
+  const labelText = `${t(TranslationKey[label as TranslationKey])}${required ? ' *' : ''}`
+
+  return (
+    <div className={cx(styles.root, { [styles.row]: isRow }, wrapperClassName)}>
+      <Tooltip title={tooltip ? t(TranslationKey[tooltip as TranslationKey]) : ''}>
+        {label ? <p className={cx(styles.label, labelClassName)}>{labelText}</p> : null}
+      </Tooltip>
+      {children}
+    </div>
+  )
+})
